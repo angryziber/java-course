c80e55d37dc5ffc5e4bcc7a8fe71210c72512f08
Igor Artemtsuk (030721) <artsept@gmail.com>
2007-01-31 15:04:01 +0000

    commit HelloWorld

---
+++ src/net/azib/java/students/t030721/HelloWorld.java
@@ -0,0 +1,18 @@
+package net.azib.java.students.t030721;
+
+/**
+ * HelloWorld
+ *
+ * @author t030721
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
