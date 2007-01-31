41e35d90122c06522e0073412dfa7c64ab26cd33
Vadim Ivanov (020543) <t020543@hot.ee>
2007-01-31 15:14:16 +0000

    vaata

---
+++ src/t020543/HelloWorld.java
@@ -0,0 +1,12 @@
+package t020543;
+
+/**
+ * HelloWorld
+ *
+ * @author t020543
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println( "Hello world!");
+	}
+}
