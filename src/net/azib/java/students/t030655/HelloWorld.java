57f672c7ccf6b411ac3dcf86548163d664f2765f
Triin Nestor (030655) <triin.nestor@mail.ee>
2007-01-31 15:06:24 +0000

    töötav HelloWorld

---
+++ src/net/azib/java/students/t030655/HelloWorld.java
@@ -0,0 +1,18 @@
+package net.azib.java.students.t030655;
+
+/**
+ * HelloWorld
+ *
+ * @author t030655
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
