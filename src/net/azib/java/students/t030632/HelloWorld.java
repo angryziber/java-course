445d8dd2d4ee4afb7088ee9c1ca82e8df7a89f59
Anton Tsertov (030632) <antonchertov@gmail.com>
2007-01-31 15:02:33 +0000

    First steps in Eclipse

---
+++ src/net/azib/java/students/t030632/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t030632;
+
+/**
+ * HelloWorld
+ *
+ * @author t030632
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, My Java World!");
+	}
+}
