589e2d5c863b0f255216790c3329447c0113e71a
Mart Viljaste (030656) <sprot@sprot.pri.ee>
2007-01-31 15:05:05 +0000

    See on siis esimene commit selle arvutiga

--- src/net/azib/java/students/t012345/HelloWorld.java
+++ src/net/azib/java/students/t012345/HelloWorld.java
@@ -3,7 +3,7 @@ package net.azib.java.students.t012345;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, cruel world!");
+		System.out.println("Tere tulemast Sprot");
 	}
 
 }
