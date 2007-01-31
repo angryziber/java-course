a3a37acf796b978f5fff5d899bc5e3dc1fa03255
Anton Keks (Tutor) <anton@azib.net>
2007-01-31 15:34:03 +0000

    restored

--- src/net/azib/java/students/t012345/HelloWorld.java
+++ src/net/azib/java/students/t012345/HelloWorld.java
@@ -3,7 +3,7 @@ package net.azib.java.students.t012345;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Tere tulemast Sprot");
+		System.out.println("Hello, cruel world!");
 	}
 
 }
