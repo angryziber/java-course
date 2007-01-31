675dc1fdcee8ae1457ccba7b04ba440ec40e1926
Astrid Pärn (060401) <astrid.parn@gmail.com>
2007-01-31 15:11:48 +0000

    added HelloWorld class

---
+++ src/net/azib/java/students/t060401/HelloWorld.java
@@ -0,0 +1,18 @@
+package net.azib.java.students.t060401;
+
+/**
+ * HelloWorld
+ *
+ * @author t060401
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
