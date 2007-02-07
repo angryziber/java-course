c7e32fc0748bbc90f72556458d42c949106c0180
Martin Kenk (030636) <martin.kenk@mail.ee>
2007-02-07 14:46:45 +0000

    comments added

--- src/net/azib/java/students/t030636/HelloWorld.java
+++ src/net/azib/java/students/t030636/HelloWorld.java
@@ -1,9 +1,14 @@
+/*
+ * package HelloWorld
+ * t030636
+ * modified: 07.02.2007
+ */
 package net.azib.java.students.t030636;
 
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hyper-super-megaproge!");
+		System.out.println("Hyper-super-megaproge! ver2");
 	}
 
 }
