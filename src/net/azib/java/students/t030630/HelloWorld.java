654b44dfbb92d9dc5e72e87bbc42c4f63ac1f258
Unknown <t030630@tud.ttu.ee>
2007-02-07 17:33:43 +0000

    introduction

---
+++ src/net/azib/java/students/t030630/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.azib.java.students.t030630;
+
+/**
+ * HelloWorld
+ *
+ * @author t030630
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Eclipse!");	
+	}
+
+}
