217acf8f7ef2f83cd71bdbc7b0730df30bfe62d2
Unknown <t050209@tud.ttu.ee>
2007-02-07 16:42:34 +0000

    teine tund

---
+++ src/net/azib/java/students/t050209/Dog.java
@@ -0,0 +1,10 @@
+package net.azib.java.students.t050209;
+
+/**
+ * Dog
+ *
+ * @author t050209
+ */
+public class Dog {
+  public String name = "unknown";
+}
---
+++ src/net/azib/java/students/t050209/NumberStaff.java
@@ -0,0 +1,45 @@
+package net.azib.java.students.t050209;
+
+import java.math.BigDecimal;
+
+/**
+ * NumberStaff
+ *
+ * @author t050209
+ */
+public class NumberStaff {
+	static int b; /* static'ut tuleks vältida */
+	/* static final on konstant */
+	
+	static final String HELLO = "abc"; /* HELLO = null */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*Dog on muutuja tüüp, dog - muutuja tüüp*/
+		Dog dog = new Dog();
+		/* Dog dog;
+		   dog = new Dog(); */
+		
+		Boolean d = new Boolean(true);
+		
+		char c = '\u0050';
+		System.out.println(c);
+		
+	    /*NumberStaff.b=7;*/ /* teises klassis olles vaja ette */
+		/*final int hello = 7;*/ /* ei saa väärtust muuta */
+	    /*System.out.println(NUmber2Stuff.accessMe); *//* sysout + ctrl + space*/
+		/*System.out.println(HELLO);*/
+		System.out.println(dog.name);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.parseInt("123", 16)); /*Hex -> dec*/ 
+	    
+		BigDecimal db = new BigDecimal("1.025");
+		BigDecimal db2 = BigDecimal.TEN;
+		System.out.println(db.add(db2));
+		
+		Object str = "aaa";
+		System.out.println(str instanceof String);
+	}
+
+}
