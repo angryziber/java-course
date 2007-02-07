a57c80ad6fd236ad1e0e6c4cadaa33fac3d3f9eb
Igor Artemtsuk (030721) <artsept@gmail.com>
2007-02-07 15:15:56 +0000

    First Number

---
+++ src/net/azib/java/students/t030721/NumberStuff.java
@@ -0,0 +1,21 @@
+package net.azib.java.students.t030721;
+
+/**
+ * NumS
+ *
+ * @author t030721
+ */
+public class NumberStuff {
+
+	static int b;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		NumberStuff.b = 5;
+		System.out.println(b);
+		
+
+	}
+
+}
