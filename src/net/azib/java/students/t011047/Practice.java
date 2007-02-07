b4eea3bca51bebaf400274d82494f19f19045fb7
Marko Käis (011047) <marko.k@estpak.ee>
2007-02-07 15:00:13 +0000

    Paketi loomine.

---
+++ src/net/azib/java/students/t011047/Practice.java
@@ -0,0 +1,18 @@
+package net.azib.java.students.t011047;
+
+/**
+ * Practice
+ *
+ * @author t011047
+ */
+public class Practice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 7;
+		a = 3;
+	}
+
+}
