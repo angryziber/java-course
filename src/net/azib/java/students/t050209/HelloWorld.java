39a933141b61bfbcbffbeaff93d5f73f41dc13ab
Unknown <t050209@tud.ttu.ee>
2007-02-07 14:35:36 +0000

    esimene harjutus

---
+++ src/net/azib/java/students/t050209/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.azib.java.students.t050209;
+
+/**
+ * HelloWorld
+ *
+ * @author t050209
+ */
+public class HelloWorld 
+{
+  public static void main(String[] args)
+  {
+	String XX = "Tere";
+	System.out.println(XX);
+  }
+}
