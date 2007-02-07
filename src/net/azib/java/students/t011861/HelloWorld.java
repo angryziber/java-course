b87ac2938226c4a39ed5abbc0d90d58ab7a0a030
Jaak Karjane (011861) <jaak.karjane@mail.ee>
2007-02-07 17:52:37 +0000

    teine tund

---
+++ src/net/azib/java/students/t011861/HelloWorld.java
@@ -0,0 +1,8 @@
+package net.azib.java.students.t011861;
+
+public class HelloWorld {
+
+	public static void main(String[] arg) {
+		System.out.println("Hello World! TÖÖTAB");
+}
+}
---
+++ src/net/azib/java/students/t011861/koer.java
@@ -0,0 +1,17 @@
+package net.azib.java.students.t011861;
+
+/**
+ * koer
+ *
+ * @author t011861
+ */
+public class koer {
+	public String name;
+	public Byte age;
+		public String getName() {
+			return name;
+		}
+	public koer(String name){
+		this.name = name;
+	}
+}
