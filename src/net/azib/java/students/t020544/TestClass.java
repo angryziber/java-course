3bde4f20e1d5892c80fb4c34b478b6481ea8cfbe
Julija Kondratjeva (020544) <julija17@gmail.com>
2007-02-10 20:34:33 +0000

    commit

---
+++ src/net/azib/java/students/t020544/TestClass.java
@@ -0,0 +1,17 @@
+package net.azib.java.students.t020544;
+
+/**
+ * TestClass
+ * 
+ * @author Julija Kondratjeva
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+
+}
