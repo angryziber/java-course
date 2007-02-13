58a73549d1a3d973b65629d033114968ed1f92fd
Julija Kondratjeva (020544) <julija17@gmail.com>
2007-02-13 12:45:28 +0000

    commit

--- src/net/azib/java/students/t020544/DogStuff.java
+++ src/net/azib/java/students/t020544/DogStuff.java
@@ -11,11 +11,24 @@ public class DogStuff {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Dog sobaka = new Dog("Deik", (byte) 3, "Doberman", "Sombody");
-		System.out.println(sobaka.getName() + " " +sobaka.getAge()+" "+sobaka.getBreed()+" "+sobaka.getOwner());
+		
+		Dog dobermanDeik = new Dog("Deik", (byte) 3, "Doberman", "Somebody");
+		
+		System.out.println("Name:" +dobermanDeik.getName() + ";  Age:" +dobermanDeik.getAge()+";  Breed:"+dobermanDeik.getBreed()+";  Owner:"+dobermanDeik.getOwner());
+		
 		for (int i=0; i< 4; i++){
-			sobaka.growUp();
+			dobermanDeik.growUp();
 		}
-		System.out.println(sobaka.getName() + " " +sobaka.getAge()+" "+sobaka.getBreed()+" "+sobaka.getOwner());
+		System.out.println(dobermanDeik.getName() + " " +dobermanDeik.getAge()+" "+dobermanDeik.getBreed()+" "+dobermanDeik.getOwner());
+
+		Dog dobermanRex = new Dog("Rex", (byte) 0, "Doberman", "Somebody");
+		
+		System.out.println(dobermanRex.getName() + " " +dobermanRex.getAge()+" "+dobermanRex.getBreed()+" "+dobermanRex.getOwner());
+		
+		for (int i=0; i< 4; i++){
+			dobermanRex.growUp();
+		}
+		System.out.println(dobermanRex.getName() + " " +dobermanRex.getAge()+" "+dobermanRex.getBreed()+" "+dobermanRex.getOwner());
+	
 	}
 }
