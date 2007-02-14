c05afe6686a0e087aade772e3691b24fce46d6d0
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-02-14 12:30:03 +0000

    some cosmetics

---
+++ src/net/azib/java/students/t020556/DocPrac.java
@@ -0,0 +1,18 @@
+package net.azib.java.students.t020556;
+
+/**
+ * For <i>documentation</i> practice
+ *
+ * @author t020556
+ */
+public class DocPrac {
+	
+	/**
+	 * Main entry popint
+	 * @param args input parameters for the main method
+	 * 
+	 */
+	public static void main(String args){
+		
+	}
+}
