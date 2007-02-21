5efb8993a46ca968c595f1a02dbbf110376eaa89
Triin Nestor (030655) <triin.nestor@mail.ee>
2007-02-21 15:02:57 +0000

    Lab3 vahepealne commit

---
+++ src/net/azib/java/students/t030655/Lab3/ExceptionGame.java
@@ -0,0 +1,39 @@
+package net.azib.java.students.t030655.Lab3;
+
+/**
+ * ExceptionGame
+ *
+ * @author t030655
+ */
+public class ExceptionGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		byte[] b = new byte[] {0};
+		processByteArray(null);
+
+	}
+
+	/**
+	 * @param b
+	 */
+	private static void processByteArray(byte[] b) {
+		try{
+			System.out.println(b[0] + b[1]);
+			throw new IllegalStateException("method end");
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("No element with index " + e.getMessage());
+		}
+		 catch (NullPointerException e) {
+			System.out.println("Passed array was null");
+		}
+		 catch (Exception e) {
+			 System.out.println("Unexpected error: " + e);
+			
+		}
+	}
+
+}
