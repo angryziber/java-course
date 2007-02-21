d8b05fdb755021aa363d641b064804af73edd1c2
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-02-21 17:10:38 +0000

    DuplicateRemover interface implementation

---
+++ src/net/azib/java/students/t020556/DuplicateRemoverImpl.java
@@ -0,0 +1,33 @@
+package net.azib.java.students.t020556;
+
+import net.azib.java.collections.DuplicateRemover;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.sun.xml.internal.stream.Entity;
+import com.sun.xml.internal.ws.wsdl.parser.Message;
+
+/**
+ * DuplicateRemoverImpl implements DuplicateRemover
+ *
+ * @author t020556
+ * 
+ */
+public class DuplicateRemoverImpl implements DuplicateRemover {
+
+	/**
+	 * @param array
+	 * 
+	 */
+
+	@Override
+	public String[] removeDuplicateStrings(String[] array) {
+		Set<String> set = new LinkedHashSet<String>(Arrays.asList(array));
+		return set.toArray(new String[0]);
+	}
+
+}
