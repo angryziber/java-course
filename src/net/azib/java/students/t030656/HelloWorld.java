232bdcabda50233aab7104c8547a6e9ee16bab35
Mart Viljaste (030656) <sprot@sprot.pri.ee>
2007-02-21 14:46:49 +0000

    working version of hello world. Fixed main head.

--- src/net/azib/java/students/t030656/HelloWorld.java
+++ src/net/azib/java/students/t030656/HelloWorld.java
@@ -2,7 +2,7 @@ package net.azib.java.students.t030656;
 
 public class HelloWorld {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		System.out.println("Tere tulemast Sprot");
 	}
 
