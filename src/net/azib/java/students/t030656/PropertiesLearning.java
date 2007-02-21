91d66cef43e8eaee936103af32794418e6510dba
Mart Viljaste (030656) <sprot@sprot.pri.ee>
2007-02-21 16:11:04 +0000

    Lisasin iteraator versiooni.

--- src/net/azib/java/students/t030656/PropertiesLearning.java
+++ src/net/azib/java/students/t030656/PropertiesLearning.java
@@ -1,5 +1,7 @@
 package net.azib.java.students.t030656;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Properties;
 
 /**
@@ -14,8 +16,11 @@ public class PropertiesLearning {
 	public static void main(String[] args) {
 		Properties sysprops = System.getProperties();
 		
-		for(Object key : System.getProperties().keySet()){
-			System.out.println(key + "=" + sysprops.get(key));
-		}
+		Collection c = sysprops.keySet();
+		
+		for(Iterator i = c.iterator(); i.hasNext(); ){
+			Object key = i.next();
+			System.out.println(key);
+		}	
 	}
 }
