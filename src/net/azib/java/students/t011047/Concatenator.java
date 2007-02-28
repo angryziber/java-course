d9af1fc885e2707347715e6fbff40641001b6256
Marko Käis (011047) <marko.k@estpak.ee>
2007-02-28 13:34:27 +0000

    UnitTest practise

---
+++ src/net/azib/java/students/t011047/Concatenator.java
@@ -0,0 +1,13 @@
+package net.azib.java.students.t011047;
+
+/**
+ * Concatenator
+ * 
+ * @author t011047
+ */
+public class Concatenator {
+	// this is a method under test
+	public String concat(String a, String b) {
+		return a + b;
+	}
+}
