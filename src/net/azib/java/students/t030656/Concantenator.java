947135ffbef7e20101e79624d48828a5bec94057
Mart Viljaste (030656) <sprot@sprot.pri.ee>
2007-02-28 13:04:29 +0000

    added new class

---
+++ src/net/azib/java/students/t030656/Concantenator.java
@@ -0,0 +1,18 @@
+package net.azib.java.students.t030656;
+
+/**
+ * Concantenator
+ *
+ * @author t030656
+ */
+public class Concantenator {
+	/**
+	 * @param strA
+	 * @param strB
+	 * @return
+	 */
+	public String concanate(String strA, String strB){
+		return strA += strB;
+	} 
+
+}
