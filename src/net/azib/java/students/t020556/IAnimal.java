c481db60806bf80f2b92d760c7e7026bdc618ac0
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-02-28 14:30:18 +0000

    Animal interface

---
+++ src/net/azib/java/students/t020556/IAnimal.java
@@ -0,0 +1,20 @@
+package net.azib.java.students.t020556;
+
+/**
+ * IAnimal
+ *
+ * @author t020556
+ */
+public interface IAnimal {
+
+	/**
+	 * @return the name
+	 */
+	public abstract String getName();
+
+	/**
+	 * @return the age
+	 */
+	public abstract byte getAge();
+
+}
