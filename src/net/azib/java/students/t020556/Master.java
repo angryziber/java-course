5861d75edd947fdbeac46c1522c8297adeb3136a
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-02-28 14:30:49 +0000

    a "dog's master" class

---
+++ src/net/azib/java/students/t020556/Master.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t020556;
+
+/**
+ * Master
+ *
+ * @author t020556
+ */
+public class Master {
+	public String callDog(IAnimal animal) {
+		return "Hello, " + animal.getName();
+	}
+}
