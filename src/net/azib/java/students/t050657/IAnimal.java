83dfb25f33788d63edd1b221d870d7ce928e9339
Boriss Shelaev (050657) <shb@hot.ee>
2007-02-28 16:14:43 +0000

    Harjutus 5 Commit

---
+++ src/net/azib/java/students/t050657/IAnimal.java
@@ -0,0 +1,16 @@
+package net.azib.java.students.t050657;
+
+/**
+ * IAnimal
+ *
+ * @author t050657
+ */
+public interface IAnimal {
+
+	public abstract String getName();
+
+	public abstract int getAge();
+
+	public abstract void growUp();
+
+}
---
+++ src/net/azib/java/students/t050657/Master.java
@@ -0,0 +1,13 @@
+package net.azib.java.students.t050657;
+
+/**
+ * Master
+ *
+ * @author t050657
+ */
+public class Master {
+
+	public String call(IAnimal animal) {
+		return "Hello " + animal.getName();
+	}
+}
