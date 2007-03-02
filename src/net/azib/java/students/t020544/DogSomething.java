b8dafe417e84e3d50176bf7761fe9afd04abef2d
Julija Kondratjeva (020544) <julija17@gmail.com>
2007-03-02 23:23:33 +0000

    class name changed

---
+++ src/net/azib/java/students/t020544/DogSomething.java
@@ -0,0 +1,25 @@
+package net.azib.java.students.t020544;
+
+/**
+ * DogTest
+ *
+ * @author Julija Kondratjeva
+ */
+public class DogSomething {
+	
+	public static void main(String[] args) {
+		Dog doberman = new Dog("Deik", (byte) 3);
+			
+		//System.out.println(doberman.getName());
+		System.out.println(doberman.hashCode());
+		
+		//Animal cat = new Animal("Name", (byte) 4);
+		//System.out.println(cat.getName());
+		
+		Dog taksa = new Dog("Deik", (byte) 3);
+		//System.out.println(taksa.getName());
+		System.out.println(taksa.hashCode());
+	
+	}
+
+}
