7a418d1c2121220ae4b6320a98cfea0846f0a6df
Julija Kondratjeva (020544) <julija17@gmail.com>
2007-03-06 11:52:09 +0000

    main method removed

--- src/net/azib/java/students/t020544/Fibonacci.java
+++ src/net/azib/java/students/t020544/Fibonacci.java
@@ -12,19 +12,20 @@ public class Fibonacci {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+		public BigInteger calculateFibonacciNumber(int number) {
 
 		BigInteger[] rezult = new BigInteger[102];
 		rezult[0] = BigInteger.ZERO;
 		rezult[1] = BigInteger.ONE;
 
 		
-		for(int i = 0; i<100; i++) {
+		for(int i = 0; i<number; i++) {
 			rezult[i+2] = rezult[i+1].add(rezult[i]);
 			//System.out.println(i + ": " + rezult[i]);
 		}
-		System.out.println("100th Fibonacci number is "+rezult[100]);
+		return rezult[number];
+//		System.out.println("100th Fibonacci number is "+rezult[100]);
 	}
 
-
+//		System.out.println(calculateFibonacciNumber(100));
 }
