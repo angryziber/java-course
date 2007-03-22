725c4f374655e966a28ea3a74b11f4a9a1fb505b
Julija Kondratjeva (020544) <julija17@gmail.com>
2007-03-22 15:53:19 +0000

    21.03.2007 labor

---
+++ test/net/azib/java/students/t020544/RegexpTest.java
@@ -0,0 +1,51 @@
+package net.azib.java.students.t020544;
+
+import static org.junit.Assert.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+
+
+/**
+ * RegexpTest
+ * 
+ * @author Julija
+ */
+public class RegexpTest {
+
+	@Test
+	public void testMatches() throws Exception {
+		assertTrue("dog".matches("[a-z]{3}"));
+		assertFalse("koer".matches("[a-z]{3}"));
+		assertFalse("a dog".matches("dog"));
+	}
+
+	@Test
+	public void testReplase() throws Exception {
+		assertEquals("dag", "dog".replaceAll("o", "a"));
+		assertEquals("HXllX, WXrld!", "Hello, World!".replaceAll("[aoe]", "X"));
+	}
+
+	@Test
+	public void testSplit() throws Exception {
+		String[] s = "Hello abc def".split("\\s+");
+		assertEquals(3, s.length);
+
+	}
+	
+	@Test
+	public void testDirect() throws Exception {
+		Pattern p = Pattern.compile("a(.)c");
+		Matcher m = p.matcher("babc");
+		assertTrue(m.find());
+		assertEquals("abc", m.group());
+		assertEquals("b", m.group(1));
+		assertEquals(1, m.start());
+		assertFalse(m.find());
+		
+	}
+
+}
