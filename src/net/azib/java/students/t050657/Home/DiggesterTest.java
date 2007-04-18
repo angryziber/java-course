b996a0861ddeeda2b759e2c805e5f0997bfd5d92
Boriss Shelaev (050657) <shb@hot.ee>
2007-04-18 08:30:32 +0000

    Digester test

---
+++ src/net/azib/java/students/t050657/Home/DiggesterTest.java
@@ -0,0 +1,50 @@
+package net.azib.java.students.t050657.Home;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.digester.Digester;
+import org.xml.sax.SAXException;
+
+/**
+ * DiggesterTest
+ *
+ * @author Boriss
+ */
+public class DiggesterTest {
+
+	public static void main(String[] args) throws SAXException, IOException{
+		
+		InputStream in = DiggesterTest.class.getResourceAsStream("books.xml");
+
+		Digester digester = new Digester();
+
+		digester.addObjectCreate("bookstore", "net.azib.java.students.t050657.Home.Bookstore");
+		
+		digester.addSetProperties("bookstore");
+		
+		digester.addObjectCreate("bookstore/book", Book.class);
+		digester.addSetNext("bookstore/book", "addBook", "net.azib.java.students.t050657.Home.Book");
+		digester.addSetNext("book/author", "addAuthor", "java.lang.String");
+		
+		digester.addSetNestedProperties("bookstore/book", "book", "title");
+		digester.addSetNestedProperties("bookstore/book", "book", "author");
+		digester.addSetNestedProperties("bookstore/book", "book", "year");
+		digester.addSetNestedProperties("bookstore/book", "book", "price");
+		
+		digester.addSetProperties("bookstore/book", "category", "book");
+		digester.addSetProperties("bookstore/book", "lang", "book");
+
+		Bookstore store = (Bookstore) digester.parse(in);
+		for(Book book : store.books) {
+			System.out.println(book.getTitle());
+			System.out.println(book.getCategory());
+			System.out.println(book.getLang());
+			System.out.println(book.getAuthor());
+			System.out.println(book.getPrice());
+			System.out.println(book.getYear());
+			System.out.println();
+		} 	
+		
+	}
+}
