ebd19b638fc95ecb3ec4e99e7a0f823a827cad48
Mart Viljaste (030656) <sprot@sprot.pri.ee>
2007-04-18 13:20:55 +0000

    Tunni harjutused

---
+++ src/net/azib/java/students/t030656/examine.java
@@ -0,0 +1,20 @@
+package net.azib.java.students.t030656;
+
+
+/**
+ * examine
+ *
+ * @author t030656
+ */
+public class examine {
+
+	/**
+	 * @param args
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 */
+	public static void main(String[] args){
+		
+	}
+
+}
