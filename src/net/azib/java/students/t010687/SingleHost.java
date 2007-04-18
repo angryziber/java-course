2868963dd7db3167706d28851faab84eda32fc14
Unknown <t010687@tud.ttu.ee>
2007-04-18 14:47:34 +0000

    Singleton

---
+++ src/net/azib/java/students/t010687/SingleHost.java
@@ -0,0 +1,39 @@
+package net.azib.java.students.t010687;
+
+/**
+ * SingleHost
+ *
+ * @author t010687
+ */
+public class SingleHost {
+	private static SingleHost UniqInstance;
+	private String Name;
+	//----------------------------------------------------------------//
+	private SingleHost(){
+		Name = "Juku";
+	}
+	
+	public static SingleHost getInstance() {
+		if (UniqInstance == null){//double check! (Y)
+			synchronized(SingleHost.class){
+				if (UniqInstance == null){
+					UniqInstance = new SingleHost();
+				}
+			}
+		}
+		return UniqInstance;
+	}
+	
+	public String getName(){
+		return Name;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(SingleHost.getInstance().getName());
+
+	}
+
+}
