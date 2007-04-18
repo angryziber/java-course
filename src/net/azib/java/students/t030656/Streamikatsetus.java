9e1de8f0e7267d9bac91898946e7263bfd7cd18b
Mart Viljaste (030656) <sprot@sprot.pri.ee>
2007-04-18 13:23:06 +0000

    parandatud warningud

--- src/net/azib/java/students/t030656/Streamikatsetus.java
+++ src/net/azib/java/students/t030656/Streamikatsetus.java
@@ -1,11 +1,6 @@
 package net.azib.java.students.t030656;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
 
 
 
