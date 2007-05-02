f2b5020d87eb90eeb19693e8f6e4bf344525ea73
Anton Keks (Tutor) <anton@azib.net>
2007-05-02 16:11:01 +0000

    lecture 13

---
+++ src/net/azib/java/lessons/collections/Circle.java
@@ -0,0 +1,46 @@
+package net.azib.java.lessons.collections;
+
+/**
+ * Circle
+ *
+ * @author anton
+ */
+public class Circle extends Shape {
+
+	/* (non-Javadoc)
+	 * @see net.azib.java.lessons.collections.Shape#area()
+	 */
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.azib.java.lessons.collections.Shape#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.azib.java.lessons.collections.Shape#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.azib.java.lessons.collections.Shape#toString()
+	 */
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/net/azib/java/lessons/logging/JavaUtilLogging.java
+++ src/net/azib/java/lessons/logging/JavaUtilLogging.java
@@ -1,5 +1,6 @@
 package net.azib.java.lessons.logging;
 
+import java.net.URL;
 import java.util.logging.Logger;
 
 /**
@@ -10,11 +11,12 @@ import java.util.logging.Logger;
 public class JavaUtilLogging {
 	public static void main(String[] args) {
 		// configure java.util.logging
-		// URL loggingConfig = JavaUtilLogging.class.getClassLoader().getResource("logging.properties");
-		// System.setProperty("java.util.logging.config.file", loggingConfig.getPath());
+		URL loggingConfig = JavaUtilLogging.class.getClassLoader().getResource("logging.properties");
+		System.setProperty("java.util.logging.config.file", loggingConfig.getPath());
 		
 		Logger log = Logger.getLogger(JavaUtilLogging.class.getName());
 		log.severe("severe");
+		log.warning("warning");
 		log.info("info");
 		log.config("config");
 		log.fine("fine");
