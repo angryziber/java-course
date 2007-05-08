cbd1ce44123edb43b27b43969bb9db26ec5cf9a5
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-05-08 12:33:37 +0000

    competitor tester initial

---
+++ src/net/azib/java/students/t020556/homework/CompetitorTest.java
@@ -0,0 +1,68 @@
+package net.azib.java.students.t020556.homework;
+
+import static org.junit.Assert.*;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.Test;
+
+
+/**
+ * CompetitorTest
+ *
+ * @author agu
+ */
+public class CompetitorTest {
+	
+	private Competitor comp = new Competitor();
+	
+	@Test
+	public void testDateOfBirth() throws ParseException {
+		DateFormat df = new SimpleDateFormat("mm.dd.yyyy");
+		String dobFormatted = "01.01.1980";
+		Date dob = df.parse(dobFormatted);
+		
+		comp.setDateOfBirth(dob);
+		assertEquals(comp.getDateOfBirth(), dob);
+		
+		comp.setDateOfBirth(dobFormatted);
+		assertEquals(comp.getDateOfBirth(), dob);
+	}
+
+	@Test
+	public void testName() {
+		String name = "Василий Пупкин";
+		
+		comp.setName(name);
+		assertEquals(name, comp.getName());		
+	}
+	
+	@Test
+	public void testLocale(){
+		String locale = "ee";
+		
+		comp.setLocale(locale);
+		
+		assertNull(comp.getLocale());
+		
+		comp.setLocale(locale.toUpperCase());
+		assertEquals(new Locale(locale), comp.getLocale());
+	}
+	
+	@Test
+	public void testEvents() throws Exception {
+		for(DecathlonEvent.Field field : DecathlonEvent.Field.values()){
+			comp.setEventResult(field, field.calculatePoints("100.4"));			
+		}
+		
+		Map<DecathlonEvent.Field, Double> m = comp.getFieldEventMap();
+		for(Map.Entry<DecathlonEvent.Field, Double> entry : m.entrySet()){
+			System.out.println(entry.getKey().getName() + " = " +entry.getValue());
+		}
+	}
+}
