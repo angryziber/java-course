32d4732de6b3a87a4eb5c24acc1437706bcaa18b
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-05-08 20:15:29 +0000

    slow but sure steps to reader definitions

--- src/net/azib/java/students/t020556/homework/IDechatlonResultReader.java
+++ src/net/azib/java/students/t020556/homework/IDechatlonResultReader.java
@@ -1,5 +1,8 @@
 package net.azib.java.students.t020556.homework;
 
+import java.io.InputStream;
+import java.util.LinkedHashSet;
+
 /**
  * IDechatlonResultReader interface exposes methods to read Dechatlon results in
  * a specific fashion
@@ -8,5 +11,5 @@ package net.azib.java.students.t020556.homework;
  * @version 1
  */
 public interface IDechatlonResultReader {
-
+	public LinkedHashSet<Competitor> readResults(InputStream input);
 }
