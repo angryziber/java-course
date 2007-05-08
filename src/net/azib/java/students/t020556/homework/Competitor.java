3870571b8351b97807d43c8bf6ad2ea21c114cc5
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-05-08 08:29:22 +0000

    competitor class

--- src/net/azib/java/students/t020556/homework/Competitor.java
+++ src/net/azib/java/students/t020556/homework/Competitor.java
@@ -1,27 +1,41 @@
 package net.azib.java.students.t020556.homework;
 
+import java.nio.charset.Charset;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.EnumMap;
+import java.util.GregorianCalendar;
 import java.util.Locale;
-
-import antlr.collections.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Competitor class resembles dechatlon competitors
  *
  * @author Agu Aarna
+ * 
  * @version 1
  */
 public class Competitor {
-	private EnumMap<DecathlonEvent.Running, Double> runningEvents = 
+	//data members
+	private Map<DecathlonEvent.Running, Double> runningEvents = 
 		new EnumMap<DecathlonEvent.Running, Double>(DecathlonEvent.Running.class);
-	private EnumMap<DecathlonEvent.Field, Double> fieldEvents = 
+	private Map<DecathlonEvent.Field, Double> fieldEvents = 
 		new EnumMap<DecathlonEvent.Field, Double>(DecathlonEvent.Field.class);
 	
-	private String name = "unknown";
+	private String name = null;
 	private Calendar dateOfBirth = null;
 	private Locale locale = null;
+
+	//functional members
+	private Charset charset = Charset.forName("UTF-8");
 	
+	//logger
+	private Logger LOG = Logger.getLogger(this.getClass().getName());
+
 	/**
 	 * setEventResult method sets the competitor event result
 	 * 
@@ -29,6 +43,8 @@ public class Competitor {
 	 * 
 	 * @param fieldEvent - specifies an event
 	 * @param result - specifies the event's result of the competitor
+	 * 
+	 * @version 1
 	 */
 	public void setEventResult(DecathlonEvent.Running event, double result){
 		runningEvents.put(event, result);
@@ -41,9 +57,152 @@ public class Competitor {
 	 * 
 	 * @param fieldEvent - specifies an event
 	 * @param result - specifies the event's result of the competitor
+	 * 
+	 * @version 1
 	 */
 	public void setEventResult(DecathlonEvent.Field event, double result){
 		fieldEvents.put(event, result);
 	}
+	
+	/**
+	 * getRunningEventMap method returns the results of runnning events as a Map
+	 * 
+	 * @author Agu Aarna
+	 * 
+	 * @param fieldEvent - specifies an event
+	 * @param result - specifies the event's result of the competitor
+	 */
+	public Map<DecathlonEvent.Running, Double> getRunningEventMap(){
+		return runningEvents;
+	}
+	
+	/**
+	 * getFieldEventMap method returns the results of field events as a Map
+	 * 
+	 * @author Agu Aarna
+	 * 
+	 * @version 1
+	 */
+	public Map<DecathlonEvent.Field, Double> getFieldEventMap(){
+		return fieldEvents;
+	}
+
+	/**
+	 * createDateOfBirth creates the birth date (from scratch if needed).
+	 * 
+	 * @author Agu Aarna
+	 * 
+	 * @param dateOfBirht the dateOfBirth
+	 * 
+	 * @version 1
+	 */
+	private void createDateOfBirth(Date dateOfBirth) {
+		if(this.dateOfBirth == null)
+			this.dateOfBirth = new GregorianCalendar(new Locale("et"));
+		this.dateOfBirth.setTime(dateOfBirth);
+	}
+
+	/**
+	 * @author Agu Aarna
+	 * 
+	 * @return the dateOfBirth
+	 * 
+	 * @version 1
+	 */
+	public Date getDateOfBirth() {
+		if(dateOfBirth == null)
+			return null;
+		return dateOfBirth.getTime();
+	}
+
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 *
+	 * @version 1
+	 */
+	public void setDateOfBirth(Date dateOfBirth) {
+		createDateOfBirth(dateOfBirth);
+	}
+
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 *
+	 * @version 1
+	 */
+	public void setDateOfBirth(String dateOfBirth) {
+		try {
+			Date date = (new SimpleDateFormat("dd.mm.yyyy")).parse(dateOfBirth);
+			createDateOfBirth(date);
+		}
+		catch (ParseException e) {
+			LOG.log(
+				Level.SEVERE, 
+				"Unable to parse given date of birth: \"" + dateOfBirth + "\"!" + 
+				"The date has to be in a format \"dd.mm.yyyy\"",
+				e);
+		}
+	}
+
+	/**
+	 * @author Agu Aarna
+	 * 
+	 * @return the locale
+	 * 
+	 * @version 1
+	 */
+	public Locale getLocale() {
+		return locale;
+	}
+
+	/**
+	 * @author Agu Aarna
+	 * 
+	 * @param locale - the locale to set
+	 * 
+	 * @version 1
+	 */
+	public void setLocale(String locale) {
+		if(locale.matches("[A-Z]{2}"))
+			this.locale = new Locale(locale.toLowerCase());
+		else
+			LOG.warning("Unable to set string \"" + locale + "\" as locale!");
+	}
+
+	/**
+	 * @author Agu Aarna
+	 * 
+	 * @param locale - the locale to set
+	 * 
+	 * @version 1
+	 */
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+
+	/**
+	 * @author Agu Aarna
+	 * 
+	 * @return the name
+	 * 
+	 * @version 1
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @author Agu Aarna
+	 * 
+	 * @param name the name to set
+	 * 
+	 * @version 1
+	 */
+	public void setName(String name) {
+		if(name == null)
+			LOG.warning("Unable to set a given name: " + name);
+		else
+			this.name = charset.encode(name).toString();
+	}
+	
 	
 }
