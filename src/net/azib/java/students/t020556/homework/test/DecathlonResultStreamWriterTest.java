6e8656e1c51a39a1e083ee33c48b220ed44d715a
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-05-10 12:45:21 +0000

    writer test initial commit

---
+++ src/net/azib/java/students/t020556/homework/test/DecathlonResultStreamWriterTest.java
@@ -0,0 +1,44 @@
+package net.azib.java.students.t020556.homework.test;
+
+import static org.junit.Assert.*;
+
+import java.io.OutputStream;
+
+import net.azib.java.students.t020556.homework.DecathlonResultStreamWriter;
+import net.azib.java.students.t020556.homework.IDecathlonResultReader;
+import net.azib.java.students.t020556.homework.IDecathlonResultWriter;
+
+import static org.easymock.EasyMock.*;
+
+import org.junit.Test;
+
+/**
+ * DecathlonResultStreamWriterTest
+ *
+ * @author agu
+ */
+public class DecathlonResultStreamWriterTest {
+	
+	private DecathlonResultStreamWriter dsw = new DecathlonResultStreamWriter();
+
+	/**
+	 * Test method for {@link net.azib.java.students.t020556.homework.DecathlonResultStreamWriter#writeResults(net.azib.java.students.t020556.homework.IDecathlonResultReader)}.
+	 */
+	@Test
+	public void testWriteResults() {
+		IDecathlonResultReader r = createMock(IDecathlonResultReader.class);
+		expect(r.readResults()).andReturn(null);
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t020556.homework.DecathlonResultStreamWriter#setFormat(boolean)}.
+	 */
+	@Test
+	public void testSetFormat() {
+		boolean CSV = false;
+		dsw.setFormat(CSV);
+		assertTrue(dsw.getFormat() == CSV);
+		
+	}
+
+}
