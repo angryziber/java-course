7a58fb0e63fb5d8282d351c4a52a818f3f371906
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-05-11 06:12:59 +0000

    xml working

--- src/net/azib/java/students/t020556/homework/DecathlonResultXmlWriter.java
+++ src/net/azib/java/students/t020556/homework/DecathlonResultXmlWriter.java
@@ -3,13 +3,31 @@ package net.azib.java.students.t020556.homework;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.PriorityQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
 import com.sun.org.apache.xml.internal.serialize.OutputFormat;
@@ -30,7 +48,8 @@ public class DecathlonResultXmlWriter implements IDecathlonResultWriter {
 	private SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
 
 	/**
-	 * @see net.azib.java.students.t020556.homework.IDecathlonResultWriter#writeResults(net.azib.java.students.t020556.homework.IDecathlonResultReader)
+	 * @see net.azib.java.students.t020556.homework.IDecathlonResultWriter#writeResults
+	 * 		(net.azib.java.students.t020556.homework.IDecathlonResultReader)
 	 */
 	public void writeResults(IDecathlonResultReader reader) {
 		Document xmlDoc = new DocumentImpl();
@@ -45,7 +64,7 @@ public class DecathlonResultXmlWriter implements IDecathlonResultWriter {
 		Competitor comp = null;
 		int i = 1;
 		while((comp = compQ.poll()) != null){
-			Element xmlComp = xmlDoc.createElement("Competitor");
+			Element xmlComp = xmlDoc.createElementNS(null, "Competitor");
 			
 			//place
 			xmlComp.appendChild(createNode("Place", "" + (i++), xmlDoc));
@@ -58,7 +77,8 @@ public class DecathlonResultXmlWriter implements IDecathlonResultWriter {
 				createNode("DateOfBirth", formatter.format(comp.getDateOfBirth()), xmlDoc));
 
 			//Locale
-			xmlComp.appendChild(createNode("Country", comp.getLocale().getCountry(), xmlDoc));
+			xmlComp.appendChild(
+				createNode("Country", comp.getLocale().toString().toUpperCase(), xmlDoc));
 			
 			//score
 			xmlComp.appendChild(
@@ -80,7 +100,7 @@ public class DecathlonResultXmlWriter implements IDecathlonResultWriter {
 			
 			XMLSerializer ser = new XMLSerializer(fos, of);
 			ser.asDOMSerializer();
-			ser.serialize(xmlDoc);
+			ser.serialize(root);
 		}
 		catch (FileNotFoundException e) {
 			LOG.log(Level.SEVERE, "Error working with file " + fName, e);
@@ -99,7 +119,7 @@ public class DecathlonResultXmlWriter implements IDecathlonResultWriter {
 	}
 	
 	private Element createNode(String name, String value, Document creater){
-		Element node = creater.createElement(name);
+		Element node = creater.createElementNS(null, name);
 		node.appendChild(creater.createTextNode(value));
 		return node;
 	}
@@ -118,4 +138,38 @@ public class DecathlonResultXmlWriter implements IDecathlonResultWriter {
 		this.fName = fName;
 	}
 
+	public void transform(OutputStream out){
+		try {
+			DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			InputStream in = DecathlonResultStreamWriter.class.getResourceAsStream(fName);
+			Document xml = db.parse(in);
+			
+			Transformer t = TransformerFactory.newInstance().newTransformer(new StreamSource(in));
+			t.transform(new DOMSource(xml), new StreamResult(out));
+			
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			System.out.println(xpath.evaluate("/", xml));
+		}
+		catch (ParserConfigurationException e) {
+			LOG.log(Level.SEVERE, "Document builder could not be found!", e);
+		}
+		catch (SAXException e) {
+			LOG.log(Level.SEVERE, "Unable to parse the set document " + fName, e);
+		}
+		catch (IOException e) {
+			LOG.log(Level.SEVERE, "Unable to parse the set document " + fName, e);
+		}
+		catch (TransformerConfigurationException e) {
+			LOG.log(Level.SEVERE, "Unable to create transformer", e);
+		}
+		catch (TransformerFactoryConfigurationError e) {
+			LOG.log(Level.SEVERE, "Unable to create transformer", e);
+		}
+		catch (TransformerException e) {
+			LOG.log(Level.SEVERE, "Unable to transform xml " + fName, e);
+		}
+		catch (XPathExpressionException e) {
+			LOG.log(Level.SEVERE, "Unable to evaluate XPATH for root element", e);
+		}
+	}
 }
