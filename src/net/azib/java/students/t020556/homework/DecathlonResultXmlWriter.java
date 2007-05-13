97420c88d67cc8280360c0696463c501b270c437
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-05-13 09:36:41 +0000

    xml writer now stream-sensitive

--- src/net/azib/java/students/t020556/homework/DecathlonResultXmlWriter.java
+++ src/net/azib/java/students/t020556/homework/DecathlonResultXmlWriter.java
@@ -3,8 +3,6 @@ package net.azib.java.students.t020556.homework;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -13,7 +11,6 @@ import java.util.PriorityQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Result;
@@ -30,7 +27,6 @@ import javax.xml.transform.stream.StreamSource;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.ProcessingInstruction;
-import org.xml.sax.SAXException;
 
 /**
  * DecathlonResultXmlWriter
@@ -43,7 +39,9 @@ public class DecathlonResultXmlWriter implements IDecathlonResultWriter {
 	private Logger LOG = Logger.getLogger(this.getClass().getName());
 
 	private URL fileUrl = DecathlonResultXmlWriter.class.getResource("/");
-	private URL transformer = DecathlonResultXmlWriter.class.getResource("style.xsl");
+	private URL htmlTransformer = DecathlonResultXmlWriter.class.getResource("style.xsl");
+	private OutputStream out = null;
+	private boolean isXmlFormat = true;
 	
 	private String rootName = "DecathlonResults";
 	private SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
@@ -56,29 +54,75 @@ public class DecathlonResultXmlWriter implements IDecathlonResultWriter {
 		writeResults(reader.readResults());
 	}
 	
-	private Element createNode(String name, String value, Document creater){
-		Element node = creater.createElement(name);
-		node.appendChild(creater.createTextNode(value));
-		return node;
-	}
-	
-	private String createFileName(URL fileURL){
-		try {
-			File file = new File(fileUrl.toURI());
-			String fName = file.getPath();
-			if(file.isDirectory()){
-				fName += File.separator + "default.xml";
-				LOG.warning("No file set! Using default file name \"" + fName + "\"");
-			}
-			return fName;
-		}
-		catch (URISyntaxException e) {
-			LOG.log(Level.SEVERE, "Failed creating file name!", e);
-			return null;
+	/**
+	 * @see net.azib.java.students.t020556.homework.IDecathlonResultWriter#
+	 * 		writeResults(java.util.PriorityQueue)
+	 */
+	public void writeResults(PriorityQueue<Competitor> compQ) {
+		if(out == null){
+			LOG.severe("Output stream not set! Unable to write formatted data!");
+			return;
 		}
 		
+		try {
+			Document xmlDoc = buildDocument(compQ);
+
+			Source src = new DOMSource(xmlDoc);
+			Result res = new StreamResult(out);
+			
+			Transformer transformer = null;
+			if(isXmlFormat){
+				transformer = TransformerFactory.newInstance().newTransformer();
+				transformer.setOutputProperty("indent", "yes");
+			}
+			else{
+				transformer = TransformerFactory.newInstance().newTransformer(
+						new StreamSource(new FileInputStream(
+							createFileName(this.htmlTransformer))));
+			}
+			
+			transformer.transform(src, res);
+		}
+		catch (FileNotFoundException e) {
+			LOG.log(Level.SEVERE, "XSL transformer " + this.htmlTransformer + " file not found", e);
+		}
+		catch (TransformerConfigurationException e) {
+			LOG.log(Level.SEVERE, "Error creating XML htmlTransformer!", e);
+		}
+		catch (TransformerFactoryConfigurationError e) {
+			LOG.log(Level.SEVERE, "Error creating XML htmlTransformer!", e);
+		}
+		catch (TransformerException e) {
+			LOG.log(Level.SEVERE, "Transformation failed!", e);
+		}
+		catch (NullPointerException e){
+			LOG.log(Level.SEVERE, "Null object referenced!", e);
+		}
 	}
-	
+
+	private Document buildDocument(PriorityQueue<Competitor> compQ){
+		try {
+			//document
+			Document xmlDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+
+			//root
+			Element root = xmlDoc.createElement(rootName);
+			xmlDoc.appendChild(root);
+			addCompetitors(compQ, xmlDoc, root);
+			
+			//xsl
+			ProcessingInstruction style = xmlDoc.createProcessingInstruction(
+					"xml-stylesheet", "type=\"text/xsl\" href=\"" + htmlTransformer + "\"");
+			root.getParentNode().insertBefore(style, root);
+			
+			return xmlDoc;
+		}
+		catch (ParserConfigurationException e) {
+			LOG.log(Level.SEVERE, "Unable to create document for XML generation!", e);
+			return null;
+		}
+	}
+
 	private void addCompetitors(
 		PriorityQueue<Competitor> compQ, Document creater, Element root){
 		
@@ -119,127 +163,59 @@ public class DecathlonResultXmlWriter implements IDecathlonResultWriter {
 			root.appendChild(xmlComp);	
 		}		
 	}
+		
+	private Element createNode(String name, String value, Document creater){
+		Element node = creater.createElement(name);
+		node.appendChild(creater.createTextNode(value));
+		return node;
+	}
+	
+	private String createFileName(URL fileURL){
+		try {
+			if(fileURL == null){
+				fileURL = DecathlonResultXmlWriter.class.getResource("/");
+			}
+			
+			File file = new File(fileUrl.toURI());
+			String fName = file.getPath();
+			if(file.isDirectory()){
+				fName += File.separator + "default.xml";
+				LOG.warning("No file set! Using default file name \"" + fName + "\"");
+			}
+			return fName;
+		}
+		catch (URISyntaxException e) {
+			LOG.log(Level.SEVERE, "Failed creating file name!", e);
+			return null;
+		}
+		
+	}
 	
 	/**
-	 * setFileName sets the file name. The file with that name will be created and in
-	 * case xsl transformation transformed into html
+	 * setStream sets the output stream. 
 	 * 
 	 * @author Agu Aarna
 	 * 
-	 * @param fURL - url representng the file
+	 * @param out - the required output stream
 	 * 
 	 * @version 1
 	 */
-	public void setFile(URL fURL){
-		this.fileUrl = fURL;
+	public void setStream(OutputStream out){
+		this.out = out;
 	}
-
+	
 	/**
-	 * transform method transforms the created xml file to an html format. Note that the
-	 * xml file has to be already created in order for it to be transformed.
-	 * 
-	 * @see net.azib.java.students.t020556.homework.DecathlonResultWriter#writeResults
-	 * 		(net.azib.java.students.t020556.homework.DecathlonResultReader)
+	 * with setFormat method you can specify the format of the output.
 	 * 
 	 * @author Agu Aarna
 	 * 
-	 * @param out - output stream
-	 * 
-	 * @version 1
+	 * @param xmlFormat - 
+	 * 		<ul>
+	 * 			<li><b>true</b> - the output will be in xml format</li>
+	 * 			<li><b>false</b> - the output will be in html format</li>
+	 * 		</ul>
 	 */
-	public void transform(OutputStream out){
-		try {
-			DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document xml = db.parse(new FileInputStream(createFileName(fileUrl)));
-			
-			Transformer t = 
-				TransformerFactory.newInstance().newTransformer(
-					new StreamSource(new FileInputStream(
-						createFileName(this.transformer))));
-			t.transform(new DOMSource(xml), new StreamResult(out));
-		}
-		catch (FileNotFoundException e) {
-			LOG.log(Level.SEVERE, "Specified XML file not found", e);
-		}
-		catch (ParserConfigurationException e) {
-			LOG.log(Level.SEVERE, "Document builder could not be found!", e);
-		}
-		catch (SAXException e) {
-			LOG.log(Level.SEVERE, "Unable to parse the set XML document", e);
-		}
-		catch (IOException e) {
-			LOG.log(Level.SEVERE, "Unable to parse the set document", e);
-		}
-		catch (TransformerConfigurationException e) {
-			LOG.log(Level.SEVERE, "Unable to create transformer", e);
-		}
-		catch (TransformerFactoryConfigurationError e) {
-			LOG.log(Level.SEVERE, "Unable to create transformer", e);
-		}
-		catch (TransformerException e) {
-			LOG.log(Level.SEVERE, "Unable to transform xml", e);
-		}
-		catch (NullPointerException e){
-			LOG.log(Level.SEVERE, "Null object referenced!", e);
-		}
-	}
-
-	/**
-	 * @see net.azib.java.students.t020556.homework.IDecathlonResultWriter#
-	 * 		writeResults(java.util.PriorityQueue)
-	 */
-	public void writeResults(PriorityQueue<Competitor> compQ) {
-		FileOutputStream fos = null;
-		try {
-			//document
-			Document xmlDoc = 
-				DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-
-			//root
-			Element root = xmlDoc.createElement(rootName);
-			xmlDoc.appendChild(root);
-			addCompetitors(compQ, xmlDoc, root);
-			
-			//xsl
-			ProcessingInstruction style = xmlDoc.createProcessingInstruction(
-					"xml-stylesheet", "type=\"text/xsl\" href=\"" + transformer + "\"");
-			root.getParentNode().insertBefore(style, root);
-
-			//output
-			fos = new FileOutputStream(createFileName(fileUrl));
-
-			Source src = new DOMSource(xmlDoc);
-			Result res = new StreamResult(fos);
-			Transformer transformer = TransformerFactory.newInstance().newTransformer();
-			transformer.setOutputProperty("indent", "yes");
-			
-			transformer.transform(src, res);
-		}
-		catch (FileNotFoundException e) {
-			LOG.log(Level.SEVERE, "Specified XML file not found", e);
-		}
-		catch (TransformerConfigurationException e) {
-			LOG.log(Level.SEVERE, "Error creating XML transformer!", e);
-		}
-		catch (TransformerFactoryConfigurationError e) {
-			LOG.log(Level.SEVERE, "Error creating XML transformer!", e);
-		}
-		catch (TransformerException e) {
-			LOG.log(Level.SEVERE, "Transformation failed!", e);
-		}
-		catch (ParserConfigurationException e) {
-			LOG.log(Level.SEVERE, "Unable to create document for XML generation!", e);
-		}
-		catch (NullPointerException e){
-			LOG.log(Level.SEVERE, "Null object referenced!", e);
-		}
-		finally{
-			try {
-				fos.close();
-			}
-			catch (IOException e) {
-				//shouldn't happen
-			}
-		}
+	public void setFormat(boolean xmlFormat){
+		this.isXmlFormat = xmlFormat;
 	}
 }
