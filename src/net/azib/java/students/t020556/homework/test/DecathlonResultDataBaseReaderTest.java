479870c2480d18c2617bddf121665c6d0a9e762e
Agu Aarna (020556) <agu.aarna@mail.ee>
2007-05-17 16:39:54 +0000

    competition specification test

--- src/net/azib/java/students/t020556/homework/test/DecathlonResultDataBaseReaderTest.java
+++ src/net/azib/java/students/t020556/homework/test/DecathlonResultDataBaseReaderTest.java
@@ -2,9 +2,13 @@ package net.azib.java.students.t020556.homework.test;
 
 import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import net.azib.java.students.t020556.homework.DecathlonResultDataBaseReader;
 
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -14,7 +18,7 @@ import org.junit.Test;
  */
 public class DecathlonResultDataBaseReaderTest {
 	
-	DecathlonResultDataBaseReader dbr = new DecathlonResultDataBaseReader();
+	private static DecathlonResultDataBaseReader dbr = new DecathlonResultDataBaseReader();
 
 	/**
 	 * Test method for 
@@ -22,9 +26,8 @@ public class DecathlonResultDataBaseReaderTest {
 	 * 		DecathlonResultDataBaseReader#initConnection(
 	 * 			java.lang.String, java.lang.String, java.lang.String)}.
 	 */
-	@Before
-	@Test
-	public void testInitConnection() {
+	@BeforeClass
+	public static void testInitConnection() {
 		dbr.initConnection("jdbc:mysql://srv.azib.net/decathlon", "java", "java");
 		assertNotNull(dbr.readResults());
 	}
@@ -32,9 +35,40 @@ public class DecathlonResultDataBaseReaderTest {
 	/**
 	 * Test method for 
 	 * {@link net.azib.java.students.t020556.homework.
+	 * 		DecathlonResultDataBaseReader#getCompetitions()}
+	 * 
+	 * and
+	 * 
+	 * {@link net.azib.java.students.t020556.homework.
+	 * 		DecathlonResultDataBaseReader#setCompetitionId(Number)}
+	 */
+	@Test
+	public void testCompetitions(){
+		try {
+			ResultSet rs = dbr.getCompetitions();
+			assertNotNull(rs);
+			assertTrue(rs.last());
+			
+			int lastCompetition = rs.getInt("id");
+			
+			dbr.setCompetitionId(lastCompetition + 1);
+			assertTrue(dbr.readResults().size() == 0);
+			
+			dbr.setCompetitionId(lastCompetition);
+			assertTrue(dbr.readResults().size() > 0);
+		}
+		catch (SQLException e) {
+			fail("Unable to iterate through competitions");
+		}
+	}
+
+	/**
+	 * Test method for 
+	 * {@link net.azib.java.students.t020556.homework.
 	 * 		DecathlonResultDataBaseReader#releaseConnection()}.
 	 */
-	public void testReleaseConnection(){
+	@AfterClass
+	public static void testReleaseConnection(){
 		dbr.releaseConnection();
 		assertNull(dbr.readResults());
 	}
