6685c9e5957af348fd135d69b8e7ffc86f5beb8c
Margus Ernits (060397) <margus.ernits@gmail.com>
2007-05-31 17:40:04 +0000

    Test

---
+++ src/net/azib/java/students/t060397/Test.java
@@ -0,0 +1,18 @@
+package net.azib.java.students.t060397;
+
+/**
+ * Test
+ *
+ * @author marcus
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is a test");
+
+	}
+
+}
