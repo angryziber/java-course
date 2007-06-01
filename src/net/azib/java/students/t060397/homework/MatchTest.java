7ea58171572170dd9894efae2ae30c207c0b45e9
Margus Ernits (060397) <margus.ernits@gmail.com>
2007-06-01 15:43:47 +0000

    initial version

---
+++ src/net/azib/java/students/t060397/homework/MatchTest.java
@@ -0,0 +1,47 @@
+package net.azib.java.students.t060397.homework;
+
+import junit.framework.TestCase;
+
+/**
+ * MatchTest
+ *
+ * @author marcus
+ */
+public class MatchTest extends TestCase {
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.Match#paint(java.awt.Graphics)}.
+	 */
+	public void testPaint() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.Match#setImages(java.awt.Image, java.awt.Image)}.
+	 */
+	public void testSetImages() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.Match#init()}.
+	 */
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.Match#stop()}.
+	 */
+	public void testStop() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.Match#endMatch(java.lang.String)}.
+	 */
+	public void testEndMatch() {
+		fail("Not yet implemented");
+	}
+
+}
