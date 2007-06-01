1e6591ae965fd7706394c2fafd57efa8b178487b
Margus Ernits (060397) <margus.ernits@gmail.com>
2007-06-01 17:10:38 +0000

    junit 3

---
+++ src/net/azib/java/students/t060397/homework/SumoAppTest.java
@@ -0,0 +1,75 @@
+package net.azib.java.students.t060397.homework;
+
+import junit.framework.TestCase;
+
+/**
+ * SumoAppTest
+ *
+ * @author marcus
+ */
+public class SumoAppTest extends TestCase {
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.SumoApp#init()}.
+	 */
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.SumoApp#start()}.
+	 */
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.SumoApp#stop()}.
+	 */
+	public void testStop() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.SumoApp#setDelay()}.
+	 */
+	public void testSetDelay() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.SumoApp#paint(java.awt.Graphics)}.
+	 */
+	public void testPaintGraphics() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.SumoApp#paintFrame(java.awt.Graphics)}.
+	 */
+	public void testPaintFrame() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.SumoApp#main(java.lang.String[])}.
+	 */
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.SumoApp#run()}.
+	 */
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.azib.java.students.t060397.homework.SumoApp#update(java.awt.Graphics)}.
+	 */
+	public void testUpdateGraphics() {
+		fail("Not yet implemented");
+	}
+
+}
