7d1c01968a0c13a766929834ff664a92c0293664
Margus Ernits (060397) <margus.ernits@gmail.com>
2007-06-08 06:04:45 +0000

    Initial tests using JUnit3:(

---
+++ test/net/azib/java/students/t060397/homework/SumoAppTest.java
@@ -0,0 +1,26 @@
+package net.azib.java.students.t060397.homework;
+
+import junit.framework.TestCase;
+
+/**
+ * SumoAppTest
+ *
+ * @author marcus
+ */
+public class SumoAppTest extends TestCase {
+	SumoApp sumo = new SumoApp();
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		//sumo.init();	  
+	}
+
+
+
+	
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	      sumo=null;
+	}
+
+}
