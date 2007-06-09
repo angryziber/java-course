892b1475b2233fa03b629fec5846f08b7434b864
Margus Ernits (060397) <margus.ernits@gmail.com>
2007-06-09 06:24:29 +0000

    initial version

---
+++ src/net/azib/java/students/t060397/homework/MathCalc.java
@@ -0,0 +1,23 @@
+package net.azib.java.students.t060397.homework;
+
+/**
+ * MathCalc class contains only Matchematical calculation
+ *
+ * @author Margus Ernits
+ */
+public class MathCalc {
+	/**
+	 * The distance between two points
+	 * @param x1 first point x coordinate
+	 * @param y1 first point y coordinate
+	 * @param x2 second point x coordinate
+	 * @param y2 second point y coordinate
+	 * @return distance between two points
+	 */
+	public static double distBetweenPoints(int x1,int y1,int x2,int y2){
+		
+		return 0.0;
+		
+	}
+
+}
