a708e796ff861f97bf64d3390f4065d144631fc4
Anton Keks (Tutor) <anton@azib.net>
2008-01-05 14:23:30 +0000

    removing 2007 stuff from trunk

--- src/net/azib/java/students/t050209/hello.js
+++
@@ -1,3 +0,0 @@
-function printHello() {
-print('hello')
-}
--- src/net/azib/java/students/t960644/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-package net.azib.java.students.t960644;
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
