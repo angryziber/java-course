5ab787d03e9e0ffee300e060ea3dc1026796ff7a
Tarmo Ojala (030633) <tarmo.ojala@mail.ee>
2008-02-09 19:12:40 +0000

    first class - HelloWorld

---
+++ src/net/azib/java/students/t030633/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t030633;
+
+/**
+ * HelloWorld
+ * 
+ * @author Tarmo Ojala
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
