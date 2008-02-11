71df6f90fb53caf1e14d60aab335820e29b197a4
Romi Agar (040719) <post@romi.ee>
2008-02-11 15:04:35 +0000

    My first java prog, wee :)

---
+++ src/net/azib/java/students/t040719/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t040719;
+
+/**
+ * HelloWorld
+ * 
+ * @author t040719
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
