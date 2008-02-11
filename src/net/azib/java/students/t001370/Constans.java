1824b407a231474d1d3c83e67392e1d7838afa66
Maksim Tamleht (001370) <maksim.tamleht@bo-soft.com>
2008-02-11 17:32:51 +0000

    constans added

---
+++ src/net/azib/java/students/t001370/Constans.java
@@ -0,0 +1,19 @@
+package net.azib.java.students.t001370;
+
+/**
+ * Constans
+ *
+ * @author t001370
+ */
+public class Constans {
+	static String HELLO;
+	static String WORLD;
+	
+	static
+	{
+		HELLO = "Hello";
+		WORLD = "World";
+	}
+	
+	static final String GREETING_MESSAGE = HELLO + ", " + WORLD;
+}
