dc90698808a601982714b2d8d2760bba6e14e8c2
Maksim Tamleht (001370) <maksim.tamleht@bo-soft.com>
2008-02-11 14:20:03 +0000

    HelloWorld app initial source code

---
+++ src/net/azib/java/students/t001370/HelloWorld.java
@@ -0,0 +1,18 @@
+package net.azib.java.students.t001370;
+
+/**
+ * HelloWorld
+ *
+ * @author t001370
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bla-bla");
+	}
+
+}
