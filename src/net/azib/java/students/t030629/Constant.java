bdfe515a3c2192b7f2f35380e6b9b8521c7ab5ff
Anton Tshepurov (030629) <anton.chepurov@gmail.com>
2008-02-11 17:38:18 +0000

    Lecture 1(week 3)

---
+++ src/net/azib/java/students/t030629/Constant.java
@@ -0,0 +1,25 @@
+package net.azib.java.students.t030629;
+
+/**
+ * Constant
+ *
+ * @author Randy
+ */
+public class Constant {
+	static final String HELLO;
+	static final String WORD;
+
+	static{
+		WORD = "WORD";
+		HELLO = "Hello";
+	}
+	
+	static final String GREETING_MESSAGE = WORD + ", " + HELLO;
+	
+	public String doSmth(){
+		String someString = null;
+		int e = 0;
+		Integer w = 5; 
+		return someString;
+	}
+}
