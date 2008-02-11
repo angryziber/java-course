2ea47bfc87e018efd5229088b1558b1f16371381
Viktor Volke (970880) <volke@hot.ee>
2008-02-11 16:21:43 +0000

    test commit1

---
+++ src/net/azib/java/students/t970880/Lesson1.java
@@ -0,0 +1,16 @@
+package net.azib.java.students.t970880;
+
+import java.util.Date;
+
+/**
+ * Lesson1
+ *
+ * @author t970880
+ */
+public class Lesson1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println(new Date());
+	}
+
+}
