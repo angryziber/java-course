2e03a01a001a7743e6e086faade6b56701abe18b
Raido Pahtma (040771) <raidoz@hot.ee>
2008-02-11 13:48:40 +0000

    HelloWorld class

---
+++ src/net/azib/java/students/t040771/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t040771;
+
+/**
+ * HelloWorld
+ * 
+ * @author t040771
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
