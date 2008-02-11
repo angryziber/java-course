3fa6c2492e622f05b13ab3182146c25e0c89d21a
Allan Berg (980814) <allan.berg@bo-soft.ee>
2008-02-11 14:04:27 +0000

    Hello world example

---
+++ src/net/azib/java/students/t980814/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t980814;
+
+/**
+ * HelloWorld
+ * 
+ * @author t980814
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
