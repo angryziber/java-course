cf2e6bb2625dee127de08d0d6fa4e9f275ea09a9
Janika Liiv (072073) <janika.liiv@gmail.com>
2008-02-18 17:23:12 +0000

    Lesson 2 example code.

---
+++ src/net/azib/java/students/t072073/MyArray.java
@@ -0,0 +1,31 @@
+package net.azib.java.students.t072073;
+
+import java.util.Scanner;
+
+/**
+ * MyArray
+ *
+ * @author t072073
+ */
+public class MyArray {
+	public static void main(String[] args) {
+		System.out.println("Sisesta number:");
+		Scanner scanner = new Scanner(System.in);
+		int i = scanner.nextInt();
+		String [] yourInput = new String [i];
+		int result = 0;
+		for (int a = 0; a < i; a++ ){
+			System.out.println("Sisesta " + (a+1) + ". string.");
+			yourInput[a] = scanner.next();		
+		}		
+		
+		//for each
+		for (String a : yourInput){
+			result += a.length();
+		}
+		System.out.println(result);
+		
+		
+	}
+
+}
