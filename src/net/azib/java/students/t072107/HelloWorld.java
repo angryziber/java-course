6737ea3b120533ecbcd18c50233653e7dfef986f
Andrei Kort?ak (072107) <akortsak@bk.ru>
2008-02-20 13:38:32 +0000

    my first class - HelloWorld

---
+++ src/net/azib/java/students/t072107/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.azib.java.students.t072107;
+
+/**
+ * HelloWorld
+ * 
+ * @author Andrei Kortsak
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
