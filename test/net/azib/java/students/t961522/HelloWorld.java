8483bb8d6280ccc45955faa85ebd4d3afeefc91c
Siim Reinaas (961522) <siimre@gmail.com>
2008-02-26 16:35:36 +0000

    my first class - HelloWorld

---
+++ test/net/azib/java/students/t961522/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t961522;
+
+/**
+ * HelloWorld
+ * 
+ * @author siim.reinaas
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
