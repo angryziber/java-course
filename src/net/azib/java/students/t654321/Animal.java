cd015af7edef150e3b90217ecbb32bda524b0ffe
Karl Bogdanov (654321) <kb@hot.ee>
2008-02-27 19:01:53 +0000

    Homework for third lesson

---
+++ src/net/azib/java/students/t654321/Animal.java
@@ -0,0 +1,53 @@
+package net.azib.java.students.t654321;
+
+/**
+ * Animal
+ *
+ * @author karl
+ */
+public abstract class Animal {
+	
+	private String name;
+	private String color;
+	private int age;
+	
+	
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+
+
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public Animal(String name, String color, int age){
+		this.name = name;
+		this.color = color;
+		this.age = age;
+	}
+	
+	abstract void makeSound();
+	abstract void jump();
+	abstract void fly();
+	
+}
