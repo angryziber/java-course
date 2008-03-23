4418930068d4fce99e8c0849baae5140c0eedadc
Anton Keks (Tutor) <anton@azib.net>
2008-03-23 22:52:44 +0000

    each test class must have test methods!!!

--- test/net/azib/java/students/t020281/hometasks/FileCopierImplTest.java
+++ test/net/azib/java/students/t020281/hometasks/FileCopierImplTest.java
@@ -1,10 +1,14 @@
 package net.azib.java.students.t020281.hometasks;
 
+import org.junit.Test;
+
 /**
  * FileCopierImplTest
  *
  * @author Trial
  */
 public class FileCopierImplTest {
-
+	@Test
+	public void todo() throws Exception {
+	}
 }
