b6b3302d276b2d1ca491f58b0e19b6a5937d6788
Julija Kondratjeva (020544) <julija17@gmail.com>
2008-03-31 11:58:51 +0000

    first class - HelloWorld

---
+++ src/net/azib/java/students/t020544/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.azib.java.students.t020544;
+
+/**
+ * HelloWorld
+ * 
+ * @author Julija Kondratjeva
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
