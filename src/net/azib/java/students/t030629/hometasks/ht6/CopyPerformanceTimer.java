d7b257a7f073defc764dc264a29079040ce7f841
Anton Tshepurov (030629) <anton.chepurov@gmail.com>
2008-04-03 13:09:54 +0000

    Made runnable on other machines as well

--- src/net/azib/java/students/t030629/hometasks/ht6/CopyPerformanceTimer.java
+++ src/net/azib/java/students/t030629/hometasks/ht6/CopyPerformanceTimer.java
@@ -4,6 +4,7 @@ import net.azib.java.lessons.io.FileCopier;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  * <br><br>User: Anton Chepurov
@@ -18,14 +19,19 @@ public class CopyPerformanceTimer {
         return System.currentTimeMillis() - startTime;
     }
 
-    public static void main(String[] args) {
-        final File src = new File("D:\\Documents and Settings\\Randy\\My Documents\\Anton\\TTU\\4 Kursus - MAGISTER\\4 Semester\\Java KEKS\\1-introduction.pdf");
-        final File dest1 = new File("D:\\Documents and Settings\\Randy\\My Documents\\Anton\\TTU\\4 Kursus - MAGISTER\\4 Semester\\Java KEKS\\1-introduction_COPY.pdf");
-        final File dest2 = new File("D:\\Documents and Settings\\Randy\\My Documents\\Anton\\TTU\\4 Kursus - MAGISTER\\4 Semester\\Java KEKS\\1-introduction_COPY2.pdf");
+    public static void main(String[] args) throws URISyntaxException, IOException {
+//        final File src = new File("D:\\Documents and Settings\\Randy\\My Documents\\Anton\\TTU\\4 Kursus - MAGISTER\\4 Semester\\Java KEKS\\1-introduction.pdf");
+//        final File dest1 = new File("D:\\Documents and Settings\\Randy\\My Documents\\Anton\\TTU\\4 Kursus - MAGISTER\\4 Semester\\Java KEKS\\1-introduction_COPY.pdf");
+//        final File dest2 = new File("D:\\Documents and Settings\\Randy\\My Documents\\Anton\\TTU\\4 Kursus - MAGISTER\\4 Semester\\Java KEKS\\1-introduction_COPY2.pdf");
+        final File src = new File(CopyPerformanceTimer.class.getResource("/java-logo.gif").toURI());
+        final File dest1 = File.createTempFile("java-logo_PERFORMANCE_COPY_1_", ".gif");
+        final File dest2 = File.createTempFile("java-logo_PERFORMANCE_COPY_2_", ".gif");
 
         new CopyPerformanceTimer().doMeasure(new SimpleCopyProgram(), src, dest1);
         new CopyPerformanceTimer().doMeasure(new BufferedCopyProgram(), src, dest2);
 
+        dest1.delete();
+        dest2.delete();
     }
 
     private void doMeasure(final FileCopier fileCopier, final File src, final File dest){
