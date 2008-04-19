47bfbd708ee937b3b56078e5615704252fc8df0b
Tarmo Ojala (030633) <tarmo.ojala@mail.ee>
2008-04-19 09:17:35 +0000

    home task 10, XML parsers

---
+++ src/net/azib/java/students/t030633/XMLParser.java
@@ -0,0 +1,14 @@
+package net.azib.java.students.t030633;
+
+import java.io.File;
+
+/**
+ * XMLParser
+ * 
+ * @author t030633
+ */
+public interface XMLParser {
+
+	public Bookstore parse(File file) throws Exception;
+
+}
