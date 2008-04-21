789632de94c15e9159d82b1ef7839b98c44234a9
Anton Keks (Tutor) <anton@azib.net>
2008-04-21 01:09:44 +0000

    compilation fix

--- src/net/azib/java/students/t072054/hometask1/Circle.java
+++ src/net/azib/java/students/t072054/hometask1/Circle.java
@@ -30,4 +30,28 @@ public class Circle extends Shape {
 		else
 			return false;
 	}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
