ed0543cd3171910ce225858f919b7d59a6e7688e
Roman Vassiljev (072054) <romeo@stv.ee>
2008-04-22 09:16:59 +0000

    Started!

---
+++ test/net/azib/java/students/t072054/hometask1/BinaryParserTest.java
@@ -0,0 +1,13 @@
+package net.azib.java.students.t072054.hometask1;
+
+/**
+ * BinaryParserTest
+ *
+ * @author r_vassiljev
+ */
+public class BinaryParserTest {
+	public static void main(){
+		
+	}
+	
+}
