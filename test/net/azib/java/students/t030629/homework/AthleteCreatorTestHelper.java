098152c24d98e78f56d4cdbfe4f77bbb42bf4996
Anton Tshepurov (030629) <anton.chepurov@gmail.com>
2008-05-19 19:32:38 +0000

    Encoding changed to UTF-8 with external program

--- test/net/azib/java/students/t030629/homework/AthleteCreatorTestHelper.java
+++ test/net/azib/java/students/t030629/homework/AthleteCreatorTestHelper.java
@@ -1,4 +1,4 @@
-package net.azib.java.students.t030629.homework;
+ï»¿package net.azib.java.students.t030629.homework;
 
 import net.azib.java.students.t030629.homework.base.Athlete;
 import net.azib.java.students.t030629.homework.io.CSVLoader;
@@ -20,7 +20,7 @@ public class AthleteCreatorTestHelper {
     }
 
     private static String[] athleteData = {
-            "\"Âàñèëèé Ïóïêèí\",21.10.1980,UA,13.43,4.35,8.64,1.50,1:06.06,19.05,24.89,2.20,33.48,6:51.01", // 3100 points
+            "\"Ğ’Ğ°ÑĞ¸Ğ»Ğ¸Ğ¹ ĞŸÑƒĞ¿ĞºĞ¸Ğ½\",21.10.1980,UA,13.43,4.35,8.64,1.50,1:06.06,19.05,24.89,2.20,33.48,6:51.01", // 3100 points
             "\"Siim Susi\",1.01.1976,EE,12.61,5.00,9.22,1.50,59.39,16.43,21.60,2.60,35.81,5:25.72",        // 4234 points
             "\"Sz?r?s Sz?k\",3.04.1972,HU,13.75,4.84,10.12,1.50,1:08.44,19.18,30.85,2.80,33.88,6:22.75"     // 3495 points
     };
