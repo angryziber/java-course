4cb5bb50081af90e00516ee328667fb990c9c9c7
Tarmo Ojala (030633) <tarmo.ojala@mail.ee>
2008-05-23 12:03:24 +0000

    database access bugs fixed

--- src/net/azib/java/students/t030633/homework/view/in/Database.java
+++ src/net/azib/java/students/t030633/homework/view/in/Database.java
@@ -6,10 +6,9 @@ import net.azib.java.students.t030633.homework.model.AthleteBuilder;
 import net.azib.java.students.t030633.homework.model.Event;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -28,42 +27,41 @@ public class Database implements Input {
 	private AthleteBuilder builder = new AthleteBuilder();
 	private Connection conn;
 
-	Database() {}
+	Database() {
+	}
 
 	Database(Connection conn) {
 		this.conn = conn;
 	}
 
-	private Connection getConnection(File properties) throws SQLException, IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(properties));
+	private Connection getConnection(InputStream properties) throws SQLException, IOException, ClassNotFoundException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(properties));
+		Class.forName("com.mysql.jdbc.Driver");
 		return DriverManager.getConnection(reader.readLine(), reader.readLine(), reader.readLine());
 	}
 
-	private ResultSet getAthletes(Connection conn, String key) throws IOException {
+	private ResultSet getAthletes(Connection conn, String key) throws SQLException {
+		// Athlete parameters
+		String sql = "SELECT athletes.name, athletes.dob, athletes.country_code, "
+				// Results (in the same order they are in Event)
+				+ "results.race_100m, results.long_jump, results.shot_put, "
+				+ "results.high_jump, results.race_400m, results.hurdles_110m, "
+				+ "results.discus_throw, results.pole_vault, results.javelin_throw, "
+				+ "results.race_1500m "
+				// Selection
+				+ "FROM athletes INNER JOIN results ON results.athlete_id = athletes.id "
+				+ "INNER JOIN competitions ON results.competition_id = competitions.id "
+				+ "WHERE competitions.id = ? OR competitions.name = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
 		try {
-			// Athlete parameters
-			String sql = "SELECT athletes.name, athletes.dob, athletes.country_code, "
-					// Results (in the same order they are in Event constants)
-					+ "results.race_100m, results.long_jump, results.shot_put, "
-					+ "results.high_jump, results.race_400m, results.hurdles_110m, "
-					+ "results.discus_throw, results.pole_vault, results.javelin_throw, "
-					+ "results.race_1500m "
-					// Selection
-					+ "FROM athletes INNER JOIN results ON results.athlete_id = athletes.id "
-					+ "INNER JOIN competitions ON results.competition_id = competitions.id "
-					+ "WHERE competitions.id = ? OR competitions.name = ?";
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			try {
-				stmt.setInt(1, Integer.parseInt(key));
-			}
-			catch (NumberFormatException e) { // Happens when search criterion is a competition name
-			}
-			stmt.setString(2, key);
-			return stmt.executeQuery();
+			stmt.setInt(1, Integer.parseInt(key));
 		}
-		catch (SQLException e) {
-			throw new IOException("Database access error.", e);
+		catch (NumberFormatException e) {
+			// Happens when search criterion is a competition name
+			stmt.setInt(1, 0);
 		}
+		stmt.setString(2, key);
+		return stmt.executeQuery();
 	}
 
 	private List<Athlete> parseAthletes(ResultSet results) throws SQLException {
@@ -73,7 +71,7 @@ public class Database implements Input {
 			builder.date(results.getDate("dob"));
 			builder.country(results.getString("country_code"));
 			int i = 4; // columns 4+ are results
-			// Results have been selected in the same order as Event constants
+			// Results have been selected in the same order as they are in Event
 			for (Event e : Event.values()) {
 				builder.addResult(e, ((double) Math.round(results.getFloat(i++) * 100) / 100));
 			}
@@ -83,25 +81,45 @@ public class Database implements Input {
 	}
 
 	public List<Athlete> read(String... search) throws IOException {
-		try {
-			if (conn == null) { // Connection has been set already when testing 
-				File properties = new File(DecathlonCalculator.class.getResource("db.properties").toURI());
-				if (!properties.exists())
-					throw new IOException("Unable to find properties file.");
+		InputStream properties = null;
+
+		if (conn == null) { // Connection has already been set when testing
+			try { // Try to open database connection
+				properties = DecathlonCalculator.class.getResourceAsStream("db.properties");
 				conn = getConnection(properties);
 			}
+			catch (SQLException e) {
+				throw new IOException("Unable to connect to database.", e);
+			}
+			catch (IOException e) {
+				throw new IOException("Unable to find properties file.", e);
+			}
+			catch (ClassNotFoundException e) {
+				throw new IOException("Database driver not found.", e);
+			}
+			finally {
+				properties.close();
+			}
+		}
+		
+		try { // Try to read athletes from the database
 			// Database read has one parameter, it should be the search
 			// criterion
 			return parseAthletes(getAthletes(conn, search[0]));
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
-			throw new IOException("Competition name or number not specified.");
+			throw new IOException("Competition name or number not specified.", e);
 		}
 		catch (SQLException e) {
-			throw new IOException("Error reading athlete from database.", e);
+			throw new IOException("Unable to read athletes from database.", e);
 		}
-		catch (URISyntaxException e) {
-			throw new IOException("Unable to find properties file.", e);
+		finally {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				throw new IOException("Unable to close database connetion.", e);
+			}
 		}
 	}
 
