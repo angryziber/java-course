1c8553557d8f3ba724705ef86fb126e1e2d6c12e
Anton Keks (Tutor) <anton@azib.net>
2009-02-12 22:14:28 +0000

    HelloWorld added

---
+++ src/net/azib/java/lessons/basic/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.lessons.basic;
+
+/**
+ * HelloWorld
+ *
+ * @author anton
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, Cruel World!");
+	}
+}
