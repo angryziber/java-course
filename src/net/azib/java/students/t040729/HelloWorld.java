4a988398b5805166f4c46f85959ccb1c73ef635f
Valle-Tõnis Elling (040729) <valleton@gmail.com>
2009-02-13 13:19:41 +0000

    Test class, helloworld.

---
+++ src/net/azib/java/students/t040729/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t040729;
+
+/**
+ * HelloWorld
+ * 
+ * @author valleton
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Oi tere-tere, maailm!\nxx\n");
+	}
+}
