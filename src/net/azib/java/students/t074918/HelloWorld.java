26f13fdf69303ee75ce2c279c5f3551a4ca24635
Mart Mangus (074918) <mart@idnet.ee>
2009-02-13 14:32:36 +0000

    Hello World ka SVN-i...

---
+++ src/net/azib/java/students/t074918/HelloWorld.java
@@ -0,0 +1,11 @@
+package net.azib.java.students.t074918;
+
+/**
+ * HelloWorld 
+ * @author Mart Mangus
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Tere tere!");
+	}
+}
