b74cc82fc8de6a9b64e6d74a1876904c1e6b7efe
Karl Bogdanov (654321) <kb@hot.ee>
2009-02-15 11:39:58 +0000

    Karl: Lecture 2

---
+++ src/net/azib/java/students/t654321/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.azib.java.students.t654321;
+
+/**
+ *
+ * @author Karl
+ */
+public class Constants {
+
+    public static final String GREETING_MESSAGE = "Tere";
+
+}
