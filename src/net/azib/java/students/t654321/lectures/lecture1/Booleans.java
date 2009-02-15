f37de00b4e54092243137fd385fd1db16b01dd4b
Karl Bogdanov (654321) <kb@hot.ee>
2009-02-15 21:53:08 +0000

    Karl: Classes from first lecture

---
+++ src/net/azib/java/students/t654321/lectures/lecture1/Booleans.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.azib.java.students.t654321.lectures.lecture1;
+
+/**
+ *
+ * @author Karl
+ */
+public class Booleans {
+
+    static boolean a(){
+        System.out.println("a");
+        return true;}
+    static boolean b(){
+        System.out.println("b");
+        return false;}
+
+    public static void main(String... args) {
+        System.out.println(a() || b());
+        System.out.println(1==2 ? "true" : false);
+    }
+}
---
+++ src/net/azib/java/students/t654321/lectures/lecture1/Dog.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.azib.java.students.t654321.lectures.lecture1;
+
+/**
+ *
+ * @author Karl
+ */
+public class Dog {
+    private String name;
+    private byte age;
+
+    public Dog(String name, byte age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "Dog name is " + name + " and age is " + age ;
+    }
+}
