4be95dd759657ec6c8edc9eae858702a34c043ee
Anton Keks (Tutor) <anton@azib.net>
2009-02-19 22:42:46 +0000

    implementation removed

--- src/net/azib/java/lessons/enums/Season.java
+++ src/net/azib/java/lessons/enums/Season.java
@@ -1,12 +1,15 @@
 package net.azib.java.lessons.enums;
 
+import net.azib.java.lessons.basic.Todo;
+
 public enum Season {
 	WINTER,
 	SPRING,
 	SUMMER,
 	AUTUMN;
 	
+	@Todo("Implement me")
 	public Season next() {
-		return values()[(this.ordinal() + 1) % values().length];
+		return null;
 	}
 }
