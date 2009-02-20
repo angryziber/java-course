5c0b64eab2ce6898128f9174ecda97e5a9360e43
Valle-Tõnis Elling (040729) <valleton@gmail.com>
2009-02-20 15:37:41 +0000

    Next try

--- src/net/azib/java/students/t040729/HelloWorld.java
+++ src/net/azib/java/students/t040729/HelloWorld.java
@@ -6,8 +6,9 @@ package net.azib.java.students.t040729;
  * @author valleton
  */
 
-//
 public class HelloWorld {
+
+
 	/**
 	 * Says hellow world few times
 	 * @param args
@@ -15,7 +16,13 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		for (int i = 1; i < 4; i++) {
-			System.out.println(i + ". hui tere-tere, maailm!\n");
+			System.out.println(i + ". Oi tere-tere, maailm!!!!!!!!!\n");
 		}
+	}
+	
+	@Override
+	public boolean equals(Object arg0) {
+		// TODO Auto-generated method stub
+		return super.equals(arg0);
 	}
 }
