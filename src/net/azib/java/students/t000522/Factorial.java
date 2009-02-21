840dc4bae62e8647ee0174da57775e8f376f7803
Juri Guljajev (000522) <juri.guljajev@gmail.com>
2009-02-21 18:49:43 +0000

    Juri: more advanced versions

--- src/net/azib/java/students/t000522/Factorial.java
+++ src/net/azib/java/students/t000522/Factorial.java
@@ -1,23 +1,21 @@
 package net.azib.java.students.t000522;
 
-import java.math.*;
+import java.math.BigInteger;
 
 public class Factorial {
-	//value of factorial
-	private static BigInteger factorial = new BigInteger("1");
-	//stores cycle of loop
-	private static BigInteger loopCounter = new BigInteger("0");
-	//index of element
-	private static int n = 10;
+	
+	public BigInteger compute(long index) {
+		//value of factorial
+		BigInteger factorial = new BigInteger("1");
+		
+		for (int i=1; i<=index; i++) {	
+			factorial = factorial.multiply(factorial.valueOf(i));
+		}
+		
+		return factorial;
+	}
 	
 	public static void main(String[] args) {
-
-		for (int i=1; i<=n; i++) {
-			
-			loopCounter = loopCounter.add(new BigInteger("1"));
-			factorial = factorial.multiply(loopCounter);
-			System.out.println(loopCounter + ":" + factorial);
-		
-		}
+		System.out.println(new Factorial().compute(100L));
 	}
 }
--- src/net/azib/java/students/t000522/Fibonacci.java
+++ src/net/azib/java/students/t000522/Fibonacci.java
@@ -1,26 +1,45 @@
 package net.azib.java.students.t000522;
 
-import java.math.*;
+import java.math.BigInteger;
+
+/**
+ *  
+ * Class for calculation of Fibonacci number
+ *
+ * check it out {@link net.azib.java.students.t000522.Factorial}
+ *
+ * @author jurrassic
+ * @since 2009
+ * 
+ */
 
 public class Fibonacci {
 
-	//holds current element value
-	private static BigInteger fibonacciCurrent = new BigInteger("0");
-	//holds previous element value
-	private static BigInteger fibonacciPrevious = new BigInteger("0");
-	//index of element
-	private static int n = 100;
+	/**
+	 * Compute fibonacci number with arbitrary precision
+	 * @param index
+	 * @return Fibonacci number with specified index
+	 */
+	
+	public BigInteger compute(long index) {
+		
+		//holds current element value
+		BigInteger fibonacciCurrent = new BigInteger("0");
+		//holds previous element value
+		BigInteger fibonacciPrevious = new BigInteger("0");
+		
+		if (index > 0) {
+			fibonacciCurrent = fibonacciCurrent.ONE;
+		}
+		
+		for (int i=2; i<=index; i++){
+			fibonacciCurrent = fibonacciCurrent.add(fibonacciPrevious);
+			fibonacciPrevious = fibonacciCurrent.subtract(fibonacciPrevious);
+		}
+		return fibonacciCurrent;
+	}
 	
 	public static void main(String[] args) {
-		for (int i=0; i<=n; i++){
-			if (i==1){
-				fibonacciCurrent = fibonacciCurrent.add(new BigInteger("1"));
-			}
-			else{
-				fibonacciCurrent = fibonacciCurrent.add(fibonacciPrevious);
-				fibonacciPrevious = fibonacciCurrent.subtract(fibonacciPrevious);
-			}
-			System.out.println(fibonacciCurrent);
-		}
+		System.out.println(new Fibonacci().compute(5L));
 	}
 }
