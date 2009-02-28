7253466eea5dd7f396e05b1afcb07436dcc3d77b
Ronald Tammepõld (090437) <ronald.tammepold@gmail.com>
2009-02-28 13:35:47 +0000

    my first Hello World!

---
+++ src/net/azib/java/students/t090437/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t090437;
+
+/**
+ * HelloWorld
+ * 
+ * @author Ronald
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
