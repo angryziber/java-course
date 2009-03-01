537b234f8531f01370842c14395d20d4bc29d0df
Juri Guljajev (000522) <juri.guljajev@gmail.com>
2009-03-01 17:13:41 +0000

    Juri: added AnimalKeeper

---
+++ src/net/azib/java/students/t000522/AnimalKeeper.java
@@ -0,0 +1,24 @@
+package net.azib.java.students.t000522;
+
+import java.util.Date;
+
+/**
+ * AnimalKeeper
+ *
+ * @author jurrassic
+ */
+public class AnimalKeeper {
+
+	public static void main(String[] args) {
+		Date birthdate = new Date();
+		birthdate.setTime(933312312312L);
+		Date takenon = new Date();
+		Animal[] animals = {new Dog("Zuchka", 10, birthdate, 1), 
+							new Cat("Kisa", 2, birthdate, takenon),
+							new Frog("Kit", 1, birthdate, 2)};
+
+		for (Animal animal : animals) {
+			System.out.println(animal);
+		}
+	}
+}
