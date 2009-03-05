32829fcc33142a177b8297e149aa3000dd312814
Juri Guljajev (000522) <juri.guljajev@gmail.com>
2009-03-05 21:35:43 +0000

    remove dependencies from other classes

--- src/net/azib/java/students/t000522/HelloWorld.java
+++ src/net/azib/java/students/t000522/HelloWorld.java
@@ -4,9 +4,5 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println(Constants.GREETING_MESSAGE);
-		//Sorry, your code was broken. Added some additional args to constructor here.
-		Dog dog = new Dog("Muki",2, null, 0);
-		System.out.println(dog);
 	}
-
 }
