d1867cae833e7a727aa0fc8912fb97911a2778fc
Anton Keks (Tutor) <anton@azib.net>
2009-03-06 14:01:51 +0000

    lesson 5 stuff

---
+++ src/net/azib/java/lessons/collections/WordFrequencyCalculatorImpl.java
@@ -0,0 +1,24 @@
+package net.azib.java.lessons.collections;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * WordFrequencyCalculator
+ *
+ * @author anton
+ */
+public class WordFrequencyCalculatorImpl implements WordFrequencyCalculator {
+
+	public Map<String, Integer> calculateFrequenciesOf(String text) {
+		String[] words = text.split("\\s");
+		Map<String, Integer> frequencies = new LinkedHashMap<String, Integer>();
+		for (String word : words) {
+			if (!frequencies.containsKey(word))
+				frequencies.put(word, 0);
+			frequencies.put(word, frequencies.get(word)+1);
+		}
+		return frequencies;
+	}
+
+}
