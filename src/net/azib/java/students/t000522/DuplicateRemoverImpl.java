f359d57fd62e382889366830ce23bb4f462a031d
Juri Guljajev (000522) <juri.guljajev@gmail.com>
2009-03-08 21:18:58 +0000

    update DuplicateRemoverImpl

--- src/net/azib/java/students/t000522/DuplicateRemoverImpl.java
+++ src/net/azib/java/students/t000522/DuplicateRemoverImpl.java
@@ -8,18 +8,15 @@ package net.azib.java.students.t000522;
 
 import net.azib.java.lessons.collections.*;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class DuplicateRemoverImpl implements DuplicateRemover{
-
+	
 	public String[] removeDuplicateStrings(String[] array) {
-		Set unsortedcollection = new HashSet();
-		for (String element : array) {
-			unsortedcollection.add(element);
-		}
-		String[] finalarray = (String[])unsortedcollection.toArray
-		(new String[unsortedcollection.size()]);
+		Set<String> unsortedcollection = new HashSet<String>(Arrays.asList(array));
+		String[] finalarray = (String[])unsortedcollection.toArray(new String[unsortedcollection.size()]);	
 		return finalarray;
 	}
 
