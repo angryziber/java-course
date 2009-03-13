2f6e90596b2237f5d2cd7c1492dfd60a49c110bb
Karl Bogdanov (654321) <kb@hot.ee>
2009-03-13 13:32:19 +0000

    Karl: Hometasks for lecture4

---
+++ src/net/azib/java/students/t654321/hometasks/lecture4/Square.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.azib.java.students.t654321.hometasks.lecture4;
+
+import net.azib.java.lessons.collections.Shape;
+
+/**
+ *
+ * @author karlb
+ */
+public class Square extends Shape {
+
+    @Override
+    public boolean equals(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int hashCode() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public double area() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
