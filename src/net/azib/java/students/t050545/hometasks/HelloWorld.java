2281d21a842661cb094cddca0902194ee4997674
Kirill Trusov (050545) <libricon@gmail.com>
2009-03-21 18:03:00 +0000

    helloworld

---
+++ src/net/azib/java/students/t050545/hometasks/HelloWorld.java
@@ -0,0 +1,18 @@
+package net.azib.java.students.t050545.hometasks;
+
+/**
+ * HelloWorld
+ * 
+ * @author kirill
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Privet");
+
+	}
+
+}
