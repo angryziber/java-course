929d5e514ab488bf929ef577edbff6d68180737f
Mart Mangus (074918) <mart@idnet.ee>
2009-04-24 15:37:10 +0000

    Lesson notes.

--- src/net/azib/java/students/t074918/FileDemo.java
+++ src/net/azib/java/students/t074918/FileDemo.java
@@ -3,7 +3,6 @@ package net.azib.java.students.t074918;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 import org.apache.commons.io.IOUtils;
 
---
+++ src/net/azib/java/students/t074918/SingeltonDemo.java
@@ -0,0 +1,15 @@
+package net.azib.java.students.t074918;
+
+/**
+ * SingeltonDemo
+ *
+ * @author mart
+ */
+public class SingeltonDemo {
+	
+	public static void main(String[] args) {
+		//Runtime r = new Runtime(); -- bad
+		Runtime r = Runtime.getRuntime();
+	}
+
+}
