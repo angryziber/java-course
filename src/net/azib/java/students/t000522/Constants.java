58ba3291ccd57acbaeea7c3e38c89cde0e26330e
Anton Keks (Tutor) <anton@azib.net>
2010-01-24 21:50:38 +0000

    removing 2009 students

--- src/net/azib/java/students/t000522/Constants.java
+++
@@ -1,5 +0,0 @@
-package net.azib.java.students.t000522;
-
-public class Constants {
-	public static final String GREETING_MESSAGE = "Lesson";
-}
--- src/net/azib/java/students/t000522/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package net.azib.java.students.t000522;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println(Constants.GREETING_MESSAGE);
-	}
-}
