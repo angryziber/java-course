469f7dcc0cb30ade5c54e28c5efc826749e37101
Kirill Strelkov (073639) <strelkov@hot.ee>
2010-02-04 18:00:05 +0000

    my first class - HelloWorld

---
+++ src/net/azib/java/students/t073639/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t073639;
+
+/**
+ * HelloWorld
+ * 
+ * @author t073639
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
