71c77f54a87631f930002bdf802c07bf89eead6a
Mihhail Arhipov (092875) <mihhail.arhipov@gmail.com>
2010-02-07 16:54:04 +0000

    Created the first test class. It outputs "Hello World!".

---
+++ src/net/azib/java/students/t092875/FirstTest.java
@@ -0,0 +1,19 @@
+package net.azib.java.students.t092875;
+
+/**
+ * FirstTest
+ *
+ * @author Mihhail
+ */
+public class FirstTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!");
+
+	}
+
+}
