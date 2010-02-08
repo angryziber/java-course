7fb47ffa94e9f4149415676fb7ccd01b9f451c60
Marek Pärnaste (050720) <Marek.Parnaste@mail.ee>
2010-02-08 19:15:26 +0000

    Test class

---
+++ src/net/azib/java/students/t050720/Supermassiv.java
@@ -0,0 +1,19 @@
+package net.azib.java.students.t050720;
+
+/**
+ * Supermassiv
+ *
+ * @author Marek
+ */
+public class Supermassiv
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Poole teraga trikid");
+	}
+
+}
