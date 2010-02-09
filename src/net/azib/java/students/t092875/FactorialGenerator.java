a24a936ca3f044efc3c0b4f77de2e9d67294b5f9
Mihhail Arhipov (092875) <mihhail.arhipov@gmail.com>
2010-02-09 20:22:49 +0000

    Done 1st home task. Created: NumbersGenerator interface - contains one abstract method, that needs to be defined in Generator classes FibonacciGenerator - implements the NumbersGenerator. Returns n-th Fibonacci number from it's only method. FactorialGenerator - implements the NumbersGenerator. Returns n-th Factorial number from it's only method. NumbersDemo - main class. the main function calls the start() method. The start method outputs the 100th Fibonacci and Factorial numbers.

---
+++ src/net/azib/java/students/t092875/FactorialGenerator.java
@@ -0,0 +1,33 @@
+package net.azib.java.students.t092875;
+
+import java.math.BigInteger;
+
+/**
+ * FactorialGenerator
+ *
+ * @author Mihhail
+ */
+public class FactorialGenerator implements NumbersGenerator {
+
+	@Override
+	public BigInteger getNthNumber(int n) {
+		if(n < 0){
+			return null;
+		}else{
+			switch(n){
+				case 0: return BigInteger.ONE;
+				case 1: return BigInteger.ONE;
+				default: return generateNthFactorial(n);
+			}
+		}
+	}
+	
+	private BigInteger generateNthFactorial(int n){
+		BigInteger temp = BigInteger.ONE;
+		for(int i=2; i<n+1; i++){
+			temp = temp.multiply(new BigInteger(""+i));
+		}
+		return temp;
+	}
+
+}
