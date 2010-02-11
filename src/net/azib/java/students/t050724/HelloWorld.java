2ec6a94eb369e83812ec178a2f7f4cb4addbfeea
Siim Somelar (050724) <siim.somelar@gmail.com>
2010-02-11 16:58:11 +0000

    my first class - HelloWorld

---
+++ src/net/azib/java/students/t050724/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.azib.java.students.t050724;
+
+/**
+ * HelloWorld
+ * 
+ * @author xom
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
