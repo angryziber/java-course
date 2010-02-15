067075edc11bc2fa979717ec8f50a260722d4037
Konstantin Shibin (092859) <konstantin.omd@gmail.com>
2010-02-15 11:26:45 +0000

    Hometask 1 - Created number factorial calculating class and result displaying class

---
+++ src/net/azib/java/students/t092859/Factorial.java
@@ -0,0 +1,21 @@
+package net.azib.java.students.t092859;
+
+import java.math.BigInteger;
+
+/**
+ * Factorial
+ *
+ * @author Konstantin
+ */
+public class Factorial {
+	
+	BigInteger getFactNum (int seqNum){
+		BigInteger result = BigInteger.ONE;
+		int i;
+		
+		for(i = 1 ; i <= seqNum; i++)
+			result = result.multiply(BigInteger.valueOf(i));
+
+		return result;
+	}
+}
---
+++ src/net/azib/java/students/t092859/NumbersDemo.java
@@ -0,0 +1,20 @@
+package net.azib.java.students.t092859;
+
+/**
+ * NumbersDemo
+ *
+ * @author Konstantin
+ */
+public class NumbersDemo {
+
+	public static void main(String[] args) {
+		
+		Fibonacci fibFinder = new Fibonacci();
+		Factorial factFinder = new Factorial();
+		
+		System.out.println("100th Fibonacci number = " + fibFinder.getFibNum(100).toString());
+		System.out.println("100! = " + factFinder.getFactNum(100).toString());
+		
+	}
+	
+}
