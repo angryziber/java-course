9fe7a592886c32ad73c87f584df14fd793be5d46
Konstantin Shibin (092859) <konstantin.omd@gmail.com>
2010-02-21 23:15:22 +0000

    Hometask 2 moved to separate package.

---
+++ src/net/azib/java/students/t092859/hometask2/Fibonacci.java
@@ -0,0 +1,30 @@
+package net.azib.java.students.t092859.hometask2;
+
+import java.math.BigInteger;
+
+/**
+ * Fibonacci
+ *
+ * @author Konstantin
+ */
+public class Fibonacci {
+	
+	BigInteger getFibNum (int seqNum){
+		BigInteger numberA = BigInteger.ZERO;
+		BigInteger numberB = BigInteger.ONE;
+		BigInteger numberTemp;
+		int i;
+		
+		for(i = 2 ; i <= seqNum; i++)
+		{
+			numberTemp = numberA.add(numberB);
+			numberA = numberB;
+			numberB = numberTemp;
+		}
+
+		if(seqNum == 0)
+			return BigInteger.ZERO;
+		return numberB;
+	}
+	
+}
