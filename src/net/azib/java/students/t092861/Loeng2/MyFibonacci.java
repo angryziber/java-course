934873336bf1c743a07e58c47fed5cb0d247255e
Stanislav Kuhtinski (092861) <csparty@hot.ee>
2010-02-23 21:13:38 +0000

    Loeng2

---
+++ src/net/azib/java/students/t092861/Loeng2/MyFibonacci.java
@@ -0,0 +1,27 @@
+package net.azib.java.students.t092861.Loeng2;
+
+/**
+ * @author Human
+ * 
+ */
+import java.math.BigInteger;
+
+class MyFibonacci {
+	BigInteger a = BigInteger.valueOf(1);
+	BigInteger b = BigInteger.valueOf(1);
+	BigInteger summ = BigInteger.valueOf(0);
+	int i;
+
+	void count() {
+		for (i = 1; i <= 100; i++) {
+			if (i <= 2)
+				summ = BigInteger.valueOf(1);
+			else
+				summ = a.add(b);
+			a = b;
+			b = summ;
+			if (i==100)
+			System.out.println(summ);
+		}
+	}
+}
