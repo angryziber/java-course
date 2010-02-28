74a1b9aa727a28a63e9fe5304db9488e95ebd61c
Mihhail Arhipov (092875) <mihhail.arhipov@gmail.com>
2010-02-28 19:51:17 +0000

    The third homework. I only created one class, which implements both Interfaces.

---
+++ src/net/azib/java/students/t092875/hw3/StringProcessor.java
@@ -0,0 +1,46 @@
+package net.azib.java.students.t092875.hw3;
+
+import net.azib.java.lessons.collections.DuplicateRemover;
+import net.azib.java.lessons.collections.WordFrequencyCalculator;
+
+import java.lang.reflect.Array;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * StringProcessor
+ *
+ * @author Mihhail
+ */
+public class StringProcessor implements DuplicateRemover, WordFrequencyCalculator {
+
+	@Override
+	public String[] removeDuplicateStrings(String... array) {
+		Set<String> set = new LinkedHashSet<String>();
+		String[] returnStrings;
+		for (String string : array) {
+			set.add(string);
+		}
+		return set.toArray(new String[set.size()]);
+	}
+
+	@Override
+	public Map calculateFrequenciesOf(String text) {
+		String[] seperatedStrings = text.split("\\s+");
+		Map<String, Integer> wordBank = new TreeMap<String, Integer>();
+		for (String string : seperatedStrings) {
+			if(wordBank.containsKey(string)){
+				Integer value = wordBank.get(string);
+				value++;
+				wordBank.put(string, value);
+			}else{
+				wordBank.put(string, 1);
+			}
+		}
+		return wordBank;
+	}
+
+}
