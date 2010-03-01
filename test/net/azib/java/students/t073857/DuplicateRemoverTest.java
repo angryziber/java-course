4ff26ed5720d31f8938de8978b23f0ff1676011c
Joonas Vali (073857) <joonas.vali@hotmail.com>
2010-03-01 19:13:47 +0000

    First tests. For homework 3.

---
+++ test/net/azib/java/students/t073857/DuplicateRemoverTest.java
@@ -0,0 +1,23 @@
+package net.azib.java.students.t073857;
+
+import static org.junit.Assert.*;
+
+import net.azib.java.students.t073857.hw3.DuplicateRemoverImpl;
+
+import org.junit.Test;
+
+/**
+ * DuplicateRemoverTest
+ *
+ * @author Joonas Vali
+ */
+public class DuplicateRemoverTest {
+	@Test
+	public void testOverall() throws Exception {
+		String[] s = {"oo", "aaa", "bbb", "ccc", "aaa", "fff", "ggg", "ggg", "aaa", "bbb"};
+		s = new DuplicateRemoverImpl().removeDuplicateStrings(s);
+		String[] exp = {"oo", "aaa", "bbb", "ccc", "fff", "ggg"};
+		assertArrayEquals("String array mismatch:", exp, s);		
+	}
+
+}
