2eca6ed078a52a8e6b6bd08e6b4a5564f4d9af96
Anton Keks (Tutor) <anton@azib.net>
2010-03-01 19:39:58 +0000

    tests moved to the correct package to avoid importing

---
+++ test/net/azib/java/students/t073857/hw3/DuplicateRemoverTest.java
@@ -0,0 +1,21 @@
+package net.azib.java.students.t073857.hw3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * DuplicateRemoverTest
+ *
+ * @author Joonas Vali
+ */
+public class DuplicateRemoverTest {
+	@Test
+	public void testOverall() throws Exception {
+		String[] s = {"oo", "aaa", "bbb", "ccc", "aaa", "fff", "ggg", "ggg", "aaa", "bbb"};
+		s = new DuplicateRemoverImpl().removeDuplicateStrings(s);
+		String[] exp = {"oo", "aaa", "bbb", "ccc", "fff", "ggg"};
+		assertArrayEquals("String array mismatch:", exp, s);		
+	}
+
+}
