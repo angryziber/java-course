af5a46818e29e372af6af0d0b6c5cc7ede2064bb
Mihkel Rämmel (100258) <rmihkel87@gmail.com>
2010-05-21 19:24:32 +0000

    t100258 homework. test(s)

---
+++ test/net/azib/java/students/t100258/Homework/CalculateResultsTest.java
@@ -0,0 +1,35 @@
+package net.azib.java.students.t100258.Homework;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+/**
+ * CalculateResultsTest
+ *
+ * @author mihkel
+ */
+public class CalculateResultsTest {
+
+	/**
+	 * Test method for {@link net.azib.java.students.t100258.Homework.CalculateResults#CalculateResults(java.util.ArrayList)}.
+	 */
+	@Test
+	public void testCalculateResults() {
+		
+		//ArrayList <Object> results = new ArrayList <Object>();
+		//Result result= new Result("Nimi" ,"0000-00-00" ,"EE" ,new Float(14.15) ,new Float(3.98) ,new Float(10.0) ,new Float(1.45) ,"70.12" ,new Float(18.65) ,new Float(29.15) ,new Float(2.15) ,new Float(32.67) ,"466.6", 0);
+		//results.add(result);
+		//CalculateResults cvb = new CalculateResults(results);
+	//assertEquals("Calculated result wrong!", new Double(2165),new Double(((Result)cvb.results.get(0)).sum));
+	
+		
+	}
+
+}
+
+
+
