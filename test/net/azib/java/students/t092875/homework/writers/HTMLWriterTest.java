1dfa23785e10d4732e99bb5272eb66ab8a58a027
Mihhail Arhipov (092875) <mihhail.arhipov@gmail.com>
2010-05-23 16:45:23 +0000

    stream test

--- test/net/azib/java/students/t092875/homework/writers/HTMLWriterTest.java
+++ test/net/azib/java/students/t092875/homework/writers/HTMLWriterTest.java
@@ -6,35 +6,44 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * HTMLWriterTest
- *
+ * 
  * @author Mihhail
  */
 public class HTMLWriterTest {
-	@Test @Ignore
-	public void  testWrite() throws Exception{
-		List<Athlete> athletes = new ArrayList<Athlete>();
-		Date dob = new Date(0);
-		String[] results = {"50.20","50.20","50.20","50.20","50.20","50.20","50.20","50.20","50.20","50.20"};
-		Athlete a = new Athlete("Mihhail Arhipov",dob,"ET",results);
+	List<Athlete> athletes;
+
+	@Before
+	public void prepareData() {
+		athletes = new ArrayList<Athlete>();
+		String[] results = { "50.20", "50.20", "50.20", "50.20", "50.20", "50.20", "50.20", "50.20", "50.20", "50.20" };
+		Athlete a = new Athlete("Mihhail Arhipov", new Date(0), "ET", results);
 		a.setPlace("1");
 		a.setResult(2000);
 		athletes.add(a);
+	}
+
+	@Test
+	public void testWrite() throws Exception {
 		HTMLWriter writer = new HTMLWriter("test_write_result.html");
 		writer.write(athletes);
 		File resultFile = new File("test_write_result.html");
-		// fails here
-		assertTrue(IOUtils.contentEquals(this.getClass().getResourceAsStream("test_write_data.html"), 
-				new FileInputStream(resultFile)));
+		
+		InputStream testStream = HTMLWriterTest.class.getResource("test_write_data.html").openStream();
+		FileInputStream resultStream = new FileInputStream(resultFile);
+		
+		assertTrue(IOUtils.contentEquals(testStream, resultStream));
 		resultFile.delete();
 	}
 }
