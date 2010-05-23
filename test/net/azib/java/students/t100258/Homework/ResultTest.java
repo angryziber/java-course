f11bf58589ddea1482457bbdbf5211538d2d8843
Mihkel Rämmel (100258) <rmihkel87@gmail.com>
2010-05-23 10:18:20 +0000

    comment

--- test/net/azib/java/students/t100258/Homework/ResultTest.java
+++ test/net/azib/java/students/t100258/Homework/ResultTest.java
@@ -12,11 +12,12 @@ import org.junit.Test;
 public class ResultTest {
 
 	/**
-	 * Test method for {@link net.azib.java.students.t100258.Homework.Result#Result(java.lang.String, java.lang.String, java.lang.String, java.lang.Float, java.lang.Float, java.lang.Float, java.lang.Float, java.lang.String, java.lang.Float, java.lang.Float, java.lang.Float, java.lang.Float, java.lang.String, int)}.
+	 * Test method for {@link net.azib.java.students.t100258.Homework.Result#Result(java.lang.String, java.lang.String, java.lang.String, java.lang.Float, java.lang.Float, java.lang.Float, java.lang.Float, java.lang.String, java.lang.Float, java.lang.Float, java.lang.Float, java.lang.Float, java.lang.String, int, int)}.
 	 */
 	@Test
 	public void testResult() {
-		// Didn't compile!!!
+		// Didn't compile!!!   
+		// Mihkel: Compiled on Eclipse 3.2.2 (latest on Debian repository) without warnings. So I didn't notice the need to box/unbox things.
 		Result result= new Result("Mingi Nimi" ,"0000-00-00" ,"EE" ,new Float(14.15) ,new Float(3.98) ,new Float(10.0) ,new Float(1.45) ,"70.12" ,new Float(18.65) ,new Float(29.15) ,new Float(2.15) ,new Float(32.67) ,"466.6", 0, 0);
 		assertEquals("Name", "Mingi Nimi", result.name);
 		assertEquals("Date", "0000-00-00", result.date);
