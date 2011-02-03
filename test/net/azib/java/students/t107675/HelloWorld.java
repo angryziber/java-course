f971d3031834ec834094691d3f3357ea5b7a45f8
Samet ERAP (107675) <samet_erap@yahoo.com>
2011-02-03 18:48:09 +0000

    My first Java programme is done :)

---
+++ test/net/azib/java/students/t107675/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.azib.java.students.t107675;
+
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World and Everybody");
+
+        Date date= new Date();
+        System.out.println(date);
+
+    }
+}
