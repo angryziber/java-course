c2cd263c724724412920c2d16db425d8d7b7d4bd
Aivar Guitar (104607) <aivar.guitar@eesti.ee>
2011-02-03 18:18:52 +0000

    harjutus: HelloWorld

---
+++ src/net/azib/java/students/t104607/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.azib.java.students.t104607;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Aivar Guitar, 104607 IASM
+ * Date: 2/3/11
+ * Time: 20:08
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hudson or Jenkins ?");
+    }
+}
