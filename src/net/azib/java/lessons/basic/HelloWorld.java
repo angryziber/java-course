622569c44dbee10addb97d1cd25ecf9fbe92ce39
Anton Keks (Tutor) <anton@azib.net>
2011-02-03 15:13:24 +0000

    lesson 1 class

--- src/net/azib/java/lessons/basic/HelloWorld.java
+++ src/net/azib/java/lessons/basic/HelloWorld.java
@@ -1,7 +1,18 @@
 package net.azib.java.lessons.basic;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        printHello();
+        printHello();
+        printHello();
+
+        Date date = new Date();
+        System.out.println(date);
+    }
+
+    private static void printHello() {
         System.out.println("Hello World!");
     }
 }
