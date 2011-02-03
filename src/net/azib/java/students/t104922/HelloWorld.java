c26ba5d49b97c92b16964acf666a6a6bdcdb3352
Erik Nöörlaid (104922) <erik.noorlaid@elion.ee>
2011-02-03 21:56:02 +0000

    Trying out the subversion thing

---
+++ src/net/azib/java/students/t104922/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.azib.java.students.t104922;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: PS1c0m
+ * Date: 3.02.11
+ * Time: 22:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
