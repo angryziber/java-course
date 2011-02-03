8308f338367463cda53d336de2942fbed80b9880
Aleksandra Madis (104965) <aleksandra.madis@gmail.com>
2011-02-03 21:40:26 +0000

    my twenty fifth class 'HelloWorld'

---
+++ src/net/azib/java/students/t104965/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t104965;
+
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Date date = new Date();
+        System.out.println("Hey, it's time now! " + date);
+    }
+}
