fc8eb0b605a399e24f276bf408ed60db4aa4ab73
Aleksandra Madis (104965) <aleksandra.madis@gmail.com>
2011-02-03 21:45:21 +0000

    the author name is added

--- src/net/azib/java/students/t104965/HelloWorld.java
+++ src/net/azib/java/students/t104965/HelloWorld.java
@@ -2,6 +2,10 @@ package net.azib.java.students.t104965;
 
 import java.util.Date;
 
+/**
+ * @author Aleksandra
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
