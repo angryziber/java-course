1327338d89e22c105e47246f8ca1cd1aab7ba8db
Robert Kullamaa (103781) <robertkullamaa@gmail.com>
2011-02-05 18:58:13 +0000

    Hello World!

---
+++ src/net/azib/java/students/t103781/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.azib.java.students.t103781;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Robert Kullamaa 103781IASB
+ * Date: 5.02.11
+ * Time: 20:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
