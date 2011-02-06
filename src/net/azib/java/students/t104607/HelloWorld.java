9552cf463f56fd89844b8805fad5779cd17428fb
Aivar Guitar (104607) <aivar.guitar@eesti.ee>
2011-02-06 22:18:35 +0000

    correct header

--- src/net/azib/java/students/t104607/HelloWorld.java
+++ src/net/azib/java/students/t104607/HelloWorld.java
@@ -1,10 +1,8 @@
 package net.azib.java.students.t104607;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Aivar Guitar, 104607 IASM
- * Date: 2/3/11
- * Time: 20:08
+ * HelloWorld
+ * @author 104607 IASM
  */
 public class HelloWorld {
     public static void main(String[] args) {
