0cfee77555f5b9f4b0559a7c7408005841a21b71
Vadim Trifonov (110013) <va.trifonov@gmail.com>
2011-02-06 18:00:16 +0000

    HelloWorld class recommit

---
+++ src/net/azib/java/students/t110013/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.azib.java.students.t110013;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Vadim
+ * Date: 6.02.11
+ * Time: 19:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
