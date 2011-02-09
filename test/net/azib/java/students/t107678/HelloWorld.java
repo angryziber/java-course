8e1670d0eec7c54daeac899a3eea5af2105cf4b8
Radomir Sebek (107678) <rasho.ieee@gmail.com>
2011-02-09 20:39:23 +0000

    HelloWorld class commited by radomir

---
+++ test/net/azib/java/students/t107678/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.azib.java.students.t107678;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Radomir
+ * Date: 2/9/11
+ * Time: 10:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
