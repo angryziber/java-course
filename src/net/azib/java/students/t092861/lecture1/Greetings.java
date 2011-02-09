f1c3f373e3d8a122ccbf810d9ae6d9fd9edd06b9
Stanislav Kuhtinski (092861) <csparty@hot.ee>
2011-02-09 16:43:05 +0000

    lecture 1 hometask

---
+++ src/net/azib/java/students/t092861/lecture1/Greetings.java
@@ -0,0 +1,22 @@
+/**
+ * @author Stanislav / 092861
+ *
+ */
+public class Greetings {
+
+	private void printHello(String s) {
+		System.out.println(s);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		
+		Greetings greetings = new Greetings();
+		greetings.printHello("Hello world 2!");
+		greetings.printHello("Hello world 3!");
+	}
+}
