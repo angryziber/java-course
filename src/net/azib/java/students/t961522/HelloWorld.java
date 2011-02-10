dc0f45430f0967fc96cd72270f1ef2a598d127aa
Siim Reinaas (961522) <siimre@gmail.com>
2011-02-10 00:17:52 +0000

    my first class

---
+++ src/net/azib/java/students/t961522/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.azib.java.students.t961522;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: siimr
+ * Date: 10.02.11
+ * Time: 2:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
