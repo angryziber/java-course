74b3f970a0476608ae33143b6103d5922a88f67b
Anton Keks (Tutor) <anton@azib.net>
2011-02-13 19:11:55 +0000

    easymock replaced by mockito

--- test/net/azib/java/questionnaire/SessionPersisterTest.java
+++ test/net/azib/java/questionnaire/SessionPersisterTest.java
@@ -1,13 +1,7 @@
 package net.azib.java.questionnaire;
 
-import static org.easymock.EasyMock.expect;
-import static org.easymock.classextension.EasyMock.createMock;
-import static org.easymock.classextension.EasyMock.replay;
-import static org.easymock.classextension.EasyMock.verify;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 import org.junit.Test;
 
 
@@ -40,18 +34,15 @@ public class SessionPersisterTest {
 	}
 	
 	@Test
-	public void dontPersistASessionTwiceWithEasyMock() throws Exception {
-		Session mockSession = createMock(Session.class);
-		expect(mockSession.isFinished()).andReturn(true).once();
-		replay(mockSession);
-		
+	public void dontPersistASessionTwiceWithMockito() throws Exception {
+		Session session = mock(Session.class);
+		when(session.isFinished()).thenReturn(true);
+
 		try {
-			new SessionPersister(mockSession);
+			new SessionPersister(session);
 			fail();
 		}
 		catch (IllegalStateException e) {			
 		}
-		
-		verify(mockSession);
 	}
 }
