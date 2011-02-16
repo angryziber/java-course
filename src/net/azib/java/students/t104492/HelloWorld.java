87d3c090649ed9919e1bba142331f4ad27d5ad31
Ain Salula (104492) <ainsalula@hotmail.com>
2011-02-16 22:06:48 +0000

    first HelloWorld class

---
+++ src/net/azib/java/students/t104492/HelloWorld.java
@@ -0,0 +1,11 @@
+package net.azib.java.students.t104492;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ain Salula 104492 IAPB 23
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
