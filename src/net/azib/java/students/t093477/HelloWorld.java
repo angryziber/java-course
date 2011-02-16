414ecbd02b46ceb968bebb47e9be4ebaff0be1b9
Sandra Vedler (093477) <sandra.vedler@gmail.com>
2011-02-16 20:48:05 +0000

    HelloWorld class

---
+++ src/net/azib/java/students/t093477/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.azib.java.students.t093477;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sandra
+ * Date: 16.02.11
+ * Time: 14:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
