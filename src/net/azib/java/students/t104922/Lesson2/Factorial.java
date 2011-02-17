cd7308d88651535dbc8afae6bffdab2f3c4ce8ce
Erik Nöörlaid (104922) <erik.noorlaid@elion.ee>
2011-02-17 10:46:58 +0000

    Factorial in non-recursive way.

---
+++ src/net/azib/java/students/t104922/Lesson2/Factorial.java
@@ -0,0 +1,22 @@
+package net.azib.java.students.t104922.Lesson2;
+
+import java.math.BigInteger;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x + "! = " + factorial(x));
+            x = x + 1;
+        }
+        while (x <= 100);
+    }
+
+    public static BigInteger factorial(int x) {
+        if (x == 0)
+            return BigInteger.ONE;
+        BigInteger fact = BigInteger.ONE;
+        for (int i = 1; i <= x; i++) fact = fact.multiply(BigInteger.valueOf(i));
+        return fact;
+    }
+}
