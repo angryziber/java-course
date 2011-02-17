65d27137366164dda641717a6c2b5d82feb13360
Malyshev Konstantin (103717) <ruzard@ruzard.com>
2011-02-17 12:18:13 +0000

    factorial up to 10000 and FibRow (working)

--- src/net/azib/java/students/t103717/fibonachi/Fibo.java
+++ src/net/azib/java/students/t103717/fibonachi/Fibo.java
@@ -1,6 +1,7 @@
 package net.azib.java.students.t103717.fibonachi;
 
 import java.math.BigInteger;
+
 /**
  * User: ruzard
  * Malyshev Konstantin 103717IAPB28
@@ -9,21 +10,28 @@ import java.math.BigInteger;
 public class Fibo {
 
 
-	static BigInteger Fnachi(int p){
-		switch (p){
-		case 1: LastPrev.setposl(BigInteger.ONE);LastPrev.setpredposl(BigInteger.ONE); return LastPrev.getPosl();
-		case 2: LastPrev.setposl(BigInteger.ONE); return LastPrev.getPosl();
-		default: break;
+	static BigInteger Fnachi(int p) {
+		switch (p) {
+			case 1:
+				LastPrev.setposl(BigInteger.ONE);
+				LastPrev.setpredposl(BigInteger.ONE);
+				return LastPrev.getPosl();
+			case 2:
+				LastPrev.setposl(BigInteger.ONE);
+				return LastPrev.getPosl();
+			default:
+				break;
 		}
 		BigInteger chislo = LastPrev.compute();
 		LastPrev.setpredposl(LastPrev.getPosl());
 		LastPrev.setposl(chislo);
-		  return chislo;
+		return chislo;
 	}
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		int i;
-		for(i = 1; i<=1000; i++){
-		System.out.print(Fnachi(i)+", ");
+		for (i = 1; i <= 10000; i++) {
+			System.out.print(Fnachi(i) + "\n\n");
 		}
 
 	}
