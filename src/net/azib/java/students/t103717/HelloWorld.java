e888b95d34f4587b2996759e6567325cb58e1621
Malyshev Konstantin (103717) <ruzard@ruzard.com>
2011-02-17 12:19:15 +0000

    factorial up to 10000 and FibRow up to 10000 (working), HelloWorld

---
+++ src/net/azib/java/students/t103717/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.azib.java.students.t103717;
+
+/**
+ * User: ruzard
+ * Malyshev Konstantin 103717IAPB28
+ * Date: 17.02.11
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
