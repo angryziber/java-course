e9a16548f5175f30c42bc450ca6d63a0e3991ae4
Anton Keks (Tutor) <anton@azib.net>
2011-02-17 12:24:12 +0000

    remove the noise

--- src/net/azib/java/students/t103597/src/NumbersDemo.java
+++ src/net/azib/java/students/t103597/src/NumbersDemo.java
@@ -3,12 +3,7 @@ package net.azib.java.students.t103597.src;
 public class NumbersDemo {
     public static void main(String[] args){
 
-        //BigInteger fibnr, factnr;
-
         // USER CAN CHOOSE THE INDEX FOR THE NUMBERS
-        //System.out.println("Enter the index of the Fibonacci & Factorial numbers you wish to see: ");
-        //Scanner index = new Scanner(System.in);
-        //System.out.println("You chose nr: "+ index.nextLine());
 
         // CREATES OBJECTS OF THE CLASSES USED
         Factorial factObject = new Factorial();
@@ -17,9 +12,6 @@ public class NumbersDemo {
         // USES SPECIFIED OBJECTS FROM CHOSEN CLASSES
         fibObject.fib();
         factObject.fact();
-
-        //System.out.println(factnr);
-        //System.out.println(x);
 
         // BRING VALUES AND OUTPUT RESULTS
 
