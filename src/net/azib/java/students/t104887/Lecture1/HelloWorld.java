c227eb8e18052f095a1148c80f3cf03e20844bb9
Juri Bogdanov (104887) <juri.bogdanov@ttu.ee>
2011-02-20 20:18:12 +0000

    Juri Bogdanov, lesson 1

---
+++ src/net/azib/java/students/t104887/Lecture1/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.azib.java.students.t104887.Lecture1;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: BJU
+ * Date: 20.02.11
+ * Time: 20:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
+
