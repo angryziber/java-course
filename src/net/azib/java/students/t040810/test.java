0ac3423d5147854a0b33832a48eab5d0cab0a134
Juri Bogdanov (104887) <juri.bogdanov@ttu.ee>
2011-02-21 21:01:15 +0000

    test

---
+++ src/net/azib/java/students/t040810/test.java
@@ -0,0 +1,11 @@
+package net.azib.java.students.t040810;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: BJU
+ * Date: 21.02.11
+ * Time: 22:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
