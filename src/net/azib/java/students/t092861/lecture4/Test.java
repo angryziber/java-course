3401bfb271e962a9d59045021f762a4874c4643b
Stanislav Kuhtinski (092861) <csparty@hot.ee>
2011-03-03 16:41:22 +0000

    Lecture 4: Exceptions, Properties, Collections Package Test

---
+++ src/net/azib/java/students/t092861/lecture4/Test.java
@@ -0,0 +1,17 @@
+package net.azib.java.students.t092861.lecture4;
+
+/**
+ * @author Stanislav / 092861
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
