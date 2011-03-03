e55cb53a40f00c542d365637e5f0366cf5b17b40
Dionis Argiri (093759) <dionis.argiri@gmail.com>
2011-03-03 23:48:53 +0000

    Added forgotten Animal class for student t109450.

---
+++ src/net/azib/java/students/t104950/Animal.java
@@ -0,0 +1,20 @@
+package net.azib.java.students.t104950;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dionis
+ * Date: 04.03.11
+ * Time: 1:45
+ * TODO: implement your own class.
+ */
+public abstract class Animal {
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	protected Animal(int age) {
+		this.age = age;
+	}
+}
