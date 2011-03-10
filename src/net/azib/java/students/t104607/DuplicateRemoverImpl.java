dbfeb987a472cb736d4a655fea196cad847e9d64
Aivar Guitar (104607) <aivar.guitar@eesti.ee>
2011-03-10 12:19:34 +0000

    homework 4: output array same order as the source array

--- src/net/azib/java/students/t104607/DuplicateRemoverImpl.java
+++ src/net/azib/java/students/t104607/DuplicateRemoverImpl.java
@@ -5,12 +5,12 @@ import net.azib.java.lessons.collections.DuplicateRemover;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class DuplicateRemoverImpl implements DuplicateRemover {
 	public String[] removeDuplicateStrings(String... array) {
 		Collection<String> set;
-		set = new HashSet<String>(Arrays.asList(array));
+		set = new LinkedHashSet<String>(Arrays.asList(array));
 		return set.toArray(new String[set.size()]);
 	}
 }
