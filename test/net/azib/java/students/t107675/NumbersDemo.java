53890017802131a80fb5e2b0a1aa6c9574c4616a
Samet ERAP (107675) <samet_erap@yahoo.com>
2011-03-13 18:05:14 +0000

    late fibonacci and factorial

---
+++ test/net/azib/java/students/t107675/NumbersDemo.java
@@ -0,0 +1,16 @@
+package net.azib.java.students.t107675;
+
+
+import java.math.BigInteger;
+
+public class NumbersDemo {
+	public static void main(String[] args) {
+		BigInteger hundred = new BigInteger("100");
+		int n=100;
+        Fibonacci.calculate(n);
+        Factorial.calculate(hundred);
+	}
+
+}
+
+
