22bc97b3d69a7d96bc499889e77da7895de894b7
Dionis Argiri (093759) <dionis.argiri@gmail.com>
2011-03-14 10:58:44 +0000

    Some lines commented out for student t107675, who forgot to implement them or at least import some implemented classes.

--- src/net/azib/java/students/t107675/hometask2/AnimalDemo.java
+++ src/net/azib/java/students/t107675/hometask2/AnimalDemo.java
@@ -4,18 +4,18 @@ package net.azib.java.students.t107675.hometask2;
 public class AnimalDemo {
 
 	 public static void main(String[] args) {
-
-		 Dog dog = new Dog("Jack ", "male" , 7);
-		 Dog babyDog = new Dog("Baby ","female",0);
-		 Bird bird = new Bird("Yumos","female");
-
-		 Animal[] animals = {dog,babyDog,bird};
-        for (Animal animal : animals) {
-            System.out.println(animal.toString());
-            animal.makeSound();
-	        System.out.println();
-
-        }
+		 //TODO: implement your classes or at least import some of them.
+//		 Dog dog = new Dog("Jack ", "male" , 7);
+//		 Dog babyDog = new Dog("Baby ","female",0);
+//		 Bird bird = new Bird("Yumos","female");
+//
+//		 Animal[] animals = {dog,babyDog,bird};
+//        for (Animal animal : animals) {
+//            System.out.println(animal.toString());
+//            animal.makeSound();
+//	        System.out.println();
+//
+//        }
 
 
 	}
--- test/net/azib/java/students/t107675/NumbersDemo.java
+++ test/net/azib/java/students/t107675/NumbersDemo.java
@@ -5,10 +5,11 @@ import java.math.BigInteger;
 
 public class NumbersDemo {
 	public static void main(String[] args) {
+		//TODO: implement your classes or at least import some of them.
 		BigInteger hundred = new BigInteger("100");
 		int n=100;
-        Fibonacci.calculate(n);
-        Factorial.calculate(hundred);
+//        Fibonacci.calculate(n);
+//        Factorial.calculate(hundred);
 	}
 
 }
