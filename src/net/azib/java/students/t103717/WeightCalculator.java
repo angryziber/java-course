b0426e915e0f3d16837ef54f7dd134e4e073914a
Malyshev Konstantin (103717) <ruzard@ruzard.com>
2011-03-17 00:26:46 +0000

    WeightCalculator

---
+++ src/net/azib/java/students/t103717/WeightCalculator.java
@@ -0,0 +1,22 @@
+package net.azib.java.students.t103717;
+
+import net.azib.java.lessons.enums.Planet;
+
+import static java.lang.System.out;
+
+/**
+ * User: ruzard
+ * Malyshev Konstantin 103717IAPB28
+ * Date: 17.03.11
+ */
+public class WeightCalculator {
+	public static void main(String[] args) {
+		double mass = 65;
+
+		for (Planet planet : Planet.values()) {
+			out.format("My planet is %s. I weight there %4.2f kg \n", planet, planet.surfaceWeight(mass) / 9.81);
+
+		}
+	}
+
+}
