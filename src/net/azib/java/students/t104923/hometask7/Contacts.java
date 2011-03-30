1a3f3015152a2e419fd147329a2e6618b648c519
Oleg East (104923) <npokc@hotmail.com>
2011-03-30 21:39:40 +0000

    Some not working stuff

---
+++ src/net/azib/java/students/t104923/hometask7/Contacts.java
@@ -0,0 +1,49 @@
+package net.azib.java.students.t104923.hometask7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Contacts {
+
+	private static List<SomePerson> contacts = new ArrayList<SomePerson>();
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Hello, do you want to add a contact? (y/n)");
+		String yesOrNo = scanner.next();
+		if (yesOrNo.equals("y")) {
+			addContact();
+		}
+		else if (yesOrNo.equals("n")) {
+
+		}
+		else if (!yesOrNo.equals("y") & !yesOrNo.equals("n")) {
+			System.out.println("Input error! (\"y\" or \"n\" only)");
+		}
+
+	}
+
+	private static void addContact() {
+		 SomePerson person = new SomePerson();
+
+			System.out.println("Contact's Name:");
+			String contactName = scanner.next("[A-Z][a-z]+\\s[A-Z][a-z]+");
+
+			person.setName(contactName);
+			System.out.println("Contact's Birthday (dd.mm.yyyy) :");
+			String contactBirthday = scanner.next("^[0-3]?[0-9]{1}\\.[0-1]{1}[0-9]{1}\\.[1,2]{1}[0,9]{1}[0-9]{2}$");
+
+			person.setBirthDay(contactBirthday);
+			System.out.println("Contact's Phone Number:");
+			String contactNumber = scanner.next("[-+\\d]");
+
+			person.setPhoneNumber(contactNumber);
+			System.out.println("Contact's E-mail:");
+			String contactEmail = scanner.next(".+@.+\\..+");
+
+			person.setEmail(contactEmail);
+			System.out.println("Do you want to add another contact? (y/n)");
+
+	}
+}
---
+++ src/net/azib/java/students/t104923/hometask7/SomePerson.java
@@ -0,0 +1,44 @@
+package net.azib.java.students.t104923.hometask7;
+
+import java.util.Date;
+
+public class SomePerson {
+
+	String name;
+	String birthDay;
+	//Date birthDay;
+	String phoneNumber;
+	String email;
+
+	public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
