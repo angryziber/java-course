a242e86736c9c8a364ac427c7d1e16159b48cd4d
Eduard Mazurtšak (104224) <e91@hot.ee>
2011-03-30 15:10:06 +0000

    my first class - HelloWorld

---
+++ src/net/azib/java/students/t104224/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.azib.java.students.t104224;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: 01
+ * Date: 30.03.11
+ * Time: 17:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello,World");
+	}
+}
