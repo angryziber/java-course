4d052fc25eea9bbe39e36704aa6f6552e144eb5b
Taavi Sonets (103640) <malinkiboi@gmail.com>
2011-03-31 17:43:00 +0000

    Fixed junk

--- src/net/azib/java/students/t103640/NumbersDemo.java
+++ src/net/azib/java/students/t103640/NumbersDemo.java
@@ -4,9 +4,9 @@ public class NumbersDemo {
 	public static void main(String[] args) {
 		Factorial facThe100 = new Factorial();
 		//TODO you forgot to remove junk after your self.
-		//FibonaciJunk fibThe100 = new FibonaciJunk();
+		Fibbonacci fibThe100 = new Fibbonacci();
 		System.out.println("Factorial of 100 is: " + facThe100.giantFac(100));
-		//System.out.println("Fibonacci swquence element #100 is: " + fibThe100.bigFiba(100));
+		System.out.println("Fibonacci swquence element #100 is: " + fibThe100.bigFiba(100));
 
 	}
 }
