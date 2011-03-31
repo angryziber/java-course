795d80caa4ef90e238141b58850af093b610d7a6
Artur Šabak (108360) <skyzer@gmail.com>
2011-03-31 17:57:42 +0000

    first class hello world

---
+++ src/net/azib/java/students/t108360/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.azib.java.students.t108360;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 31.03.11
+ * Time: 20:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("---------------------");
+	}
+}
