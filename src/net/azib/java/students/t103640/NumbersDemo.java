fbf5d3ad36ead0cb0ffd7ef499d4f70c120d80e1
Dionis Argiri (093759) <dionis.argiri@gmail.com>
2011-03-31 01:35:33 +0000

    Cleaning up some junk, which produces errors...

--- src/net/azib/java/students/t103640/NumbersDemo.java
+++ src/net/azib/java/students/t103640/NumbersDemo.java
@@ -1,13 +1,12 @@
 package net.azib.java.students.t103640;
 
-import java.math.BigInteger;
-
 public class NumbersDemo {
-    public static void main(String[] args) {
-          Factorial facThe100 = new Factorial();
-            FibonaciJunk fibThe100 = new FibonaciJunk();
-        System.out.println("Factorial of 100 is: "+ facThe100.giantFac(100));
-        System.out.println("Fibonacci swquence element #100 is: " + fibThe100.bigFiba(100));
+	public static void main(String[] args) {
+		Factorial facThe100 = new Factorial();
+		//TODO you forgot to remove junk after your self.
+		//FibonaciJunk fibThe100 = new FibonaciJunk();
+		System.out.println("Factorial of 100 is: " + facThe100.giantFac(100));
+		//System.out.println("Fibonacci swquence element #100 is: " + fibThe100.bigFiba(100));
 
-    }
+	}
 }
