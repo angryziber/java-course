e646dc40933a8e698a38183f8c541e3c0bd322c1
Dionis Argiri (093759) <dionis.argiri@gmail.com>
2011-04-07 08:59:13 +0000

    SimpleCopyProgram created using TDD approach.

---
+++ src/net/azib/java/students/t093759/hometasks/seventh/SimpleCopyProgram.java
@@ -0,0 +1,39 @@
+package net.azib.java.students.t093759.hometasks.seventh;
+
+import net.azib.java.lessons.io.DataCopier;
+import net.azib.java.lessons.io.FileCopier;
+
+import java.io.*;
+
+/**
+ * @author dionis
+ *         4/7/11 9:00 AM
+ */
+public class SimpleCopyProgram implements DataCopier, FileCopier {
+	@Override
+	public void copy(InputStream src, OutputStream dest) throws IOException {
+		if (src == null || dest == null) {
+			throw new IllegalArgumentException();
+		}
+		int oneByteAsInt;
+		while ((oneByteAsInt = src.read()) != -1) {
+			dest.write(oneByteAsInt);
+		}
+		dest.flush();
+	}
+
+	@Override
+	public void copy(File src, File dest) throws IOException {
+		if (src == null || dest == null) {
+			throw new IllegalArgumentException();
+		}
+		Reader reader = new FileReader(src);
+		Writer writer = new FileWriter(dest);
+		int oneByteAsInt;
+		while ((oneByteAsInt = reader.read()) != -1) {
+			writer.write(oneByteAsInt);
+		}
+		writer.close();
+		reader.close();
+	}
+}
