3bc63ee415a5896c5504416a7399115efd6cc000
Martin Taimre (092855) <taimre@hotmail.com>
2011-04-30 13:56:45 +0000

    added unit tests for Factorial

---
+++ test/net/azib/java/students/t092855/FactorialTest.java
@@ -0,0 +1,42 @@
+package net.azib.java.students.t092855;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+
+public class FactorialTest {
+	@Test
+	public void firstNumberIsCorrect() {
+		assertEquals(new BigInteger("1"), new Factorial().getNumber(0));
+	}
+
+	@Test
+	public void secondNumberIsCorrect() {
+		assertEquals(BigInteger.ONE, new Factorial().getNumber(1));
+	}
+
+	@Test
+	public void thirdNumberIsCorrect() {
+		assertEquals(BigInteger.valueOf(2), new Factorial().getNumber(2));
+	}
+
+	@Test
+	public void negativeNumberIsHandledCorrectly() {
+		assertEquals(new BigInteger("-1"), new Factorial().getNumber(-40));
+	}
+
+	@Test
+	public void someBigNumberIsCorrect() {
+		assertEquals(new BigInteger("2432902008176640000"), new Factorial().getNumber(20));
+		System.out.println("Some big number is tested");
+	}
+
+
+	@Test @Ignore
+	public void thisIsIgnored() {
+	}
+
+}
