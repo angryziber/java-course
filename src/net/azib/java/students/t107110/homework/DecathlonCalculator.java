23ecaf91dc38efb4bc1d35feab690629943b3716
Eduard Shustrov (107110) <eshustrov@gmail.com>
2011-05-10 01:34:27 +0000

    home work: logging is added

--- src/net/azib/java/students/t107110/homework/DecathlonCalculator.java
+++ src/net/azib/java/students/t107110/homework/DecathlonCalculator.java
@@ -1,14 +1,19 @@
 package net.azib.java.students.t107110.homework;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author Eduard Shustrov
  */
 public class DecathlonCalculator {
+	private static final Logger LOG = Logger.getLogger(DecathlonCalculator.class);
+
 	public static void main(final String[] args) {
 		try {
 			new CommandLineParser(args);
 		} catch (Exception e) {
-			System.err.println(e.getMessage());
+//			System.err.println(e.getMessage());
+			LOG.fatal(e.getMessage(), e);
 		}
 	}
 }
