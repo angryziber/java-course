64e8660552a27e7a43924d1f19521493e4f8c693
Artjom Kruglenkov (092877) <artjom.kruglenkov@gmail.com>
2011-05-13 18:48:17 +0000

    refactoring

---
+++ src/net/azib/java/students/t092877/homework/Xml.java
@@ -0,0 +1,13 @@
+package net.azib.java.students.t092877.homework;
+
+
+import java.io.File;
+import java.util.List;
+
+public class Xml {
+
+	public static void output(List<Athlete> athletes, File pathname, boolean outputToHtml) {
+
+
+	}
+}
