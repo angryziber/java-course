f068de5f5ecf1a02a1244a63994aaa28aecdca8d
Radomir Sebek (107678) <rasho.ieee@gmail.com>
2011-05-14 15:08:09 +0000

    Basic skeleton in Decathlon  class implemented, all combination CSV - CONSOLE works at this instance of time

--- src/net/azib/java/students/t107678/homework/DecathlonCalculator.java
+++ src/net/azib/java/students/t107678/homework/DecathlonCalculator.java
@@ -7,34 +7,121 @@ public class DecathlonCalculator {
     final static String CONSOLE = "-console";
     final static String CSV = "-csv";
     final static String DB = "-db";
+    final static String XML = "-xml";
+    final static String HTML = "-html";
 
 
     public static void main(String[] args) throws IOException, RecordFormatException {
 
-        if (args[0].trim().compareToIgnoreCase(CONSOLE ) == 0) {
+        if (args[0].trim().compareToIgnoreCase(CONSOLE) == 0) {      //our input is CONSOLE
 
-        } else if (args[0].trim().compareToIgnoreCase(CSV ) == 0) {
-            CSVReader reader = new CSVReader(args[1]);
-            if (args[2].trim().compareToIgnoreCase(CSV ) == 0) {
-                CSVWriter writer = new CSVWriter(args[3]);
-                ResultsComputation resultsComputation = new ResultsComputation();
-                resultsComputation.readRecords(reader);
-                resultsComputation.computeRecordsResults();
+            ConsoleReader reader = new ConsoleReader();
+
+            ResultsComputation resultsComputation = new ResultsComputation();
+            resultsComputation.readRecords(reader);
+            resultsComputation.computeRecordsResults();
+
+            if (args[1].trim().compareToIgnoreCase(CONSOLE) == 0) {
+
+                ConsoleWriter writer = new ConsoleWriter();
                 writer.writeOutput(resultsComputation);
-                System.out.println("Check the output :)");
-            }
-            if (args[2] == CONSOLE) {
-                //
-
-            }
-            if (args[2] == DB) {
-                //
             }
 
-        } else if (args[0] == DB) {
+            if (args[1].trim().compareToIgnoreCase(CSV) == 0) {
+
+                CSVWriter writer = new CSVWriter(args[2]);
+                writer.writeOutput(resultsComputation);
+                System.out.println("Decathlon results are stored in specified file");  //to be removed in final code
+            }
+
+            if (args[1].trim().compareToIgnoreCase(XML) == 0) {
+
+                XMLWriter writer = new XMLWriter(args[2]);
+                writer.writeOutput(resultsComputation);
+                System.out.println("Decathlon results are stored in specified XML file");  //to be removed in final code
+
+            }
+
+            if (args[1].trim().compareToIgnoreCase(HTML) == 0) {
+
+                HTMLWriter writer = new HTMLWriter(args[2]);
+                writer.writeOutput(resultsComputation);
+                System.out.println("Decathlon results are stored in specified HTML file");  //to be removed in final code
+            }
+
+
+        } else if (args[0].trim().compareToIgnoreCase(CSV) == 0) {        //Our input is CSV file
+
+
+            CSVReader reader = new CSVReader(args[1]);
+
+            ResultsComputation resultsComputation = new ResultsComputation();
+            resultsComputation.readRecords(reader);
+            resultsComputation.computeRecordsResults();
+
+            if (args[2].trim().compareToIgnoreCase(CSV) == 0) {
+
+                CSVWriter writer = new CSVWriter(args[3]);
+                writer.writeOutput(resultsComputation);
+                System.out.println("Decathlon results are stored in specified CSV file");  //to be removed in final code
+            }
+            if (args[2].trim().compareToIgnoreCase(CONSOLE) == 0) {
+
+                ConsoleWriter writer = new ConsoleWriter();
+                writer.writeOutput(resultsComputation);
+            }
+            if (args[2].trim().compareToIgnoreCase(XML) == 0) {
+
+                XMLWriter writer = new XMLWriter(args[3]);
+                writer.writeOutput(resultsComputation);
+                System.out.println("Decathlon results are stored in specified XML file");  //to be removed in final code
+
+            }
+            if (args[2].trim().compareToIgnoreCase(HTML) == 0) {
+
+                HTMLWriter writer = new HTMLWriter(args[3]);
+                writer.writeOutput(resultsComputation);
+                System.out.println("Decathlon results are stored in specified HTML file");  //to be removed in final code
+            }
+
+
+        } else if (args[0].trim().compareToIgnoreCase(DB) == 0) {   //We take data from database
+
+            DataBaseReader reader = new DataBaseReader();
+
+            ResultsComputation resultsComputation = new ResultsComputation();
+            resultsComputation.readRecords(reader);
+            resultsComputation.computeRecordsResults();
+
+            if (args[2].trim().compareToIgnoreCase(CONSOLE) == 0) {
+
+                ConsoleWriter writer = new ConsoleWriter();
+                writer.writeOutput(resultsComputation);
+            }
+            if (args[2].trim().compareToIgnoreCase(CSV) == 0) {
+
+                CSVWriter writer = new CSVWriter(args[3]);
+                writer.writeOutput(resultsComputation);
+                System.out.println("Decathlon results are stored in specified CSV  file");  //to be removed in final code
+            }
+            if (args[2].trim().compareToIgnoreCase(XML) == 0) {
+
+                XMLWriter writer = new XMLWriter(args[3]);
+                writer.writeOutput(resultsComputation);
+                System.out.println("Decathlon results are stored in specified XML file");  //to be removed in final code
+
+            }
+
+            if (args[2].trim().compareToIgnoreCase(HTML) == 0) {
+
+                HTMLWriter writer = new HTMLWriter(args[3]);
+                writer.writeOutput(resultsComputation);
+                System.out.println("Decathlon results are stored in specified HTML file");  //to be removed in final code
+            }
+
 
         } else {
-            System.out.println("The input method is not recognized");
+            System.out.println("The input or output is not recognized/specified correctly. Try again!");
         }
 
 
--- src/net/azib/java/students/t107678/homework/Event.java
+++ src/net/azib/java/students/t107678/homework/Event.java
@@ -3,6 +3,9 @@ package net.azib.java.students.t107678.homework;
 interface Event {
 
     public int getPoints();
+    public double getPerformance();
+    public String getEventName();
+    public String getEventUnits();
 
 
 
