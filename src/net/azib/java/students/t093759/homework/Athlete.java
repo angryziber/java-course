0fbbec83561a2f8b73a0c04032da7c65facc28a0
Dionis Argiri (093759) <dionis.argiri@gmail.com>
2011-05-18 14:27:57 +0000

    More tests for athlete class.

--- src/net/azib/java/students/t093759/homework/Athlete.java
+++ src/net/azib/java/students/t093759/homework/Athlete.java
@@ -1,7 +1,8 @@
 package net.azib.java.students.t093759.homework;
 
+import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.Locale;
 
 /**
  * @author dionis
@@ -11,28 +12,114 @@ public class Athlete {
 	private String name;
 	private Calendar dateOfBirth;
 	private String countryISO2LetterCode;
-	private Long oneHundredMeterSprintInSeconds;
-	private Long longJumpInMeter;
-	private Long shotPutInMeter;
-	private Long fourHundredMeterSprintInSeconds;
-	private Long oneHundredTenMeterHurdlesInSeconds;
-	private Long discussThrowInMeter;
-	private Long poleVaultInMeter;
-	private Long javelinThrowInMeter;
-	private Long thousandFiveHundredMeterRaceInSeconds;
+	private Double oneHundredMeterSprintInSeconds;
+	private Double longJumpInMeter;
+	private Double shotPutInMeter;
+	private Double fourHundredMeterSprintInSeconds;
+	private Double oneHundredTenMeterHurdlesInSeconds;
+	private Double discusThrowInMeter;
+	private Double poleVaultInMeter;
+	private Double javelinThrowInMeter;
+	private Double thousandFiveHundredMeterRaceInSeconds;
+
+	private Athlete() {
+	}
 
 	public static class Builder {
+		private Athlete athlete = new Athlete();
+		public final static double ONE_DAY_IN_SECONDS = 24.0 * 60.0 * 60.0;
+
 		public Builder name(String name) {
 			for (char c : name.toCharArray()) {
 				if (!Character.isLetter(c) && !Character.isSpaceChar(c))
-					throw new IllegalArgumentException("Name should consist only from characters and spaces.");
+					throw new IllegalArgumentException("Name should consist only from characters and spaces.");//TODO move to messages
 			}
+			athlete.name = name;
 			return this;
 		}
 
 		public Builder setDateOfBirth(Calendar dateOfBirth) {
-			if(!dateOfBirth.before(Calendar.getInstance()))
-				throw new IllegalArgumentException("Date of birth can not be equal or more than today's date.");
+			dateOfBirth = (Calendar) dateOfBirth.clone();
+
+			Calendar oneHundredTwentyYearsAgo = Calendar.getInstance();
+			oneHundredTwentyYearsAgo.add(Calendar.YEAR, -120);
+
+			Calendar today = Calendar.getInstance();
+
+			if (dateOfBirth.before(oneHundredTwentyYearsAgo) || dateOfBirth.after(today))
+				throw new IllegalArgumentException("Date of birth should be between today - 120 years and today.");
+
+			athlete.dateOfBirth = dateOfBirth;
+			return this;
+		}
+
+		public Builder setCountryISO2LetterCode(String countryISO2LetterCode) {
+			if (!Arrays.asList(Locale.getISOCountries()).contains(countryISO2LetterCode))
+				throw new IllegalArgumentException("It's not a country ISO2 letter code.");//TODO move to messages
+			athlete.countryISO2LetterCode = countryISO2LetterCode;
+			return this;
+		}
+
+		public Builder setFourHundredMeterSprint(double timeInSeconds) {
+			if (timeInSeconds < 0.0 || timeInSeconds >= ONE_DAY_IN_SECONDS)
+				throw new IllegalArgumentException("400 meter sprint time length should be 0 or more seconds and less than 24 hours.");//TODO move to messages
+			athlete.fourHundredMeterSprintInSeconds = timeInSeconds;
+			return this;
+		}
+
+		public Builder setOneHundredMeterSprint(double timeInSeconds) {
+			if (timeInSeconds < 0.0 || timeInSeconds >= ONE_DAY_IN_SECONDS)
+				throw new IllegalArgumentException("100 meter sprint time length should be 0 or more seconds and less than 24 hours.");//TODO move to messages
+			athlete.oneHundredMeterSprintInSeconds = timeInSeconds;
+			return this;
+		}
+
+		public Builder setLongJump(double lengthInMeter) {
+			if (lengthInMeter < 0.0 || lengthInMeter >= 20.0)
+				throw new IllegalArgumentException("Long jump should be 0 or more meters and less than 20 meters in length.");//TODO move to messages
+			athlete.longJumpInMeter = lengthInMeter;
+			return this;
+		}
+
+		public Builder setShotPut(double lengthInMeter) {
+			if (lengthInMeter < 0.0 || lengthInMeter >= 40.0)
+				throw new IllegalArgumentException("Shot put should be 0 or more meters and less than 20 meters in length.");//TODO move to messages
+			athlete.shotPutInMeter = lengthInMeter;
+			return this;
+		}
+
+		public Builder setOneHundredMeterHurdles(double timeInSeconds) {
+			if (timeInSeconds < 0.0 || timeInSeconds >= ONE_DAY_IN_SECONDS)
+				throw new IllegalArgumentException("110 m hurdles should be 0 or more seconds and less than 24 hours.");//TODO move to messages
+			athlete.oneHundredTenMeterHurdlesInSeconds = timeInSeconds;
+			return this;
+		}
+
+		public Builder setDiscusThrow(double lengthInMeters) {
+			if (lengthInMeters < 0.0 || lengthInMeters >= 200.0)
+				throw new IllegalArgumentException("Discus throw should be zero or more meter length and less than 200 meter length.");//TODO move to messages
+			athlete.discusThrowInMeter = lengthInMeters;
+			return this;
+		}
+
+		public Builder setPoleVault(double heightInMeters) {
+			if (heightInMeters < 0.0 || heightInMeters >= 20.0)
+				throw new IllegalArgumentException("Pole vault should be zero or more meter in height and less than 20 meter in height.");//TODO move to messages
+			athlete.poleVaultInMeter = heightInMeters;
+			return this;
+		}
+
+		public Builder setJavelinThrow(double lengthInMeters) {
+			if (lengthInMeters < 0.0 || lengthInMeters >= 150.0)
+				throw new IllegalArgumentException("Javelin throw should be zero or more meter length and less than 150 meter length.");//TODO move to messages
+			athlete.javelinThrowInMeter = lengthInMeters;
+			return this;
+		}
+
+		public Builder setThousandFiveHundredMeterRace(double timeInSeconds) {
+			if (timeInSeconds < 0.0 || timeInSeconds >= ONE_DAY_IN_SECONDS)
+				throw new IllegalArgumentException("1500 m race time should be 0 or more seconds and less than 24 hours.");//TODO move to messages
+			athlete.thousandFiveHundredMeterRaceInSeconds = timeInSeconds;
 			return this;
 		}
 	}
--- test/net/azib/java/students/t093759/homework/AthleteTest.java
+++ test/net/azib/java/students/t093759/homework/AthleteTest.java
@@ -2,13 +2,22 @@ package net.azib.java.students.t093759.homework;
 
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Calendar;
+
+import static net.azib.java.students.t093759.homework.Athlete.Builder.ONE_DAY_IN_SECONDS;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author dionis
  *         5/15/11 11:55 AM
  */
 public class AthleteTest {
+	@Test
+	public void oneDayInSeconds() {
+		assertThat(ONE_DAY_IN_SECONDS, equalTo(24.0 * 60.0 * 60.0));
+	}
+
 	@Test(expected = IllegalArgumentException.class)
 	public void athleteNameShouldConsistOnlyFromCharacters() {
 		new Athlete.Builder().name("Param Pam 123");
@@ -23,15 +32,111 @@ public class AthleteTest {
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void dateOfBirthShouldBeLessThanCurrentDate() {
-		new Athlete.Builder().setDateOfBirth(Calendar.getInstance());
+	public void dateOfBirthCanNotBeInFuture() {
+		Calendar futureDate = Calendar.getInstance();
+		futureDate.add(Calendar.DAY_OF_YEAR, 1);
+		new Athlete.Builder().setDateOfBirth(futureDate);
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void peopleDoNotLiveMoreThan120Years() {
 		Calendar dateInPast = Calendar.getInstance();
-		dateInPast.add(Calendar.YEAR, -120);
+		dateInPast.add(Calendar.YEAR, -121);
 		new Athlete.Builder().setDateOfBirth(dateInPast);
 	}
-	//	System.out.println(Arrays.asList(Locale.getISOCountries()));
+
+	@Test(expected = IllegalArgumentException.class)
+	public void countryISO2LetterIsChecked() {
+		new Athlete.Builder().setCountryISO2LetterCode("WTF");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void fourHundredMeterSprintTimeLengthShouldBeZeroOrMore() {
+		new Athlete.Builder().setFourHundredMeterSprint(-1.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void fourHundredMeterSprintTimeLengthShouldBeLessThan24Hours() {
+		new Athlete.Builder().setFourHundredMeterSprint(ONE_DAY_IN_SECONDS);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void oneHundredMeterSprintInSecondsShouldBeZeroOrMore() {
+		new Athlete.Builder().setOneHundredMeterSprint(-1.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void oneHundredMeterSprintTimeLengthShouldBeLessThan24Hours() {
+		new Athlete.Builder().setOneHundredMeterSprint(ONE_DAY_IN_SECONDS);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void longJumpShouldBeZeroOrMoreMetersLength() {
+		new Athlete.Builder().setLongJump(-1.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void longJumpShouldBeLessThan20MetersLength() {
+		new Athlete.Builder().setLongJump(20.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shotPutShouldBeZeroOrMoreMetersLength() {
+		new Athlete.Builder().setShotPut(-1.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shotPutShouldBeLessThan40MetersLength() {
+		new Athlete.Builder().setShotPut(40.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void oneHundredTenMeterHurdlesShouldBeZeroSecondsOrMore() {
+		new Athlete.Builder().setOneHundredMeterHurdles(-1.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void oneHundredTenMeterHurdlesShouldBeLessThan24Hours() {
+		new Athlete.Builder().setOneHundredMeterHurdles(ONE_DAY_IN_SECONDS);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void discusThrowShouldBeZeroOrMoreMeters() {
+		new Athlete.Builder().setDiscusThrow(-1.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void discusThrowShouldBeLessThan200Meter() {
+		new Athlete.Builder().setDiscusThrow(200.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void poleVaultHeightShouldBeZeroOrMoreMeters() {
+		new Athlete.Builder().setPoleVault(-1.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void poleVaultHeightShouldBeLessThan20Meter() {
+		new Athlete.Builder().setPoleVault(20.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void javelinThrowShouldBeZeroOrMoreMeter() {
+		new Athlete.Builder().setJavelinThrow(-1.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void javelinThrowShouldBeLessThan150Meter() {
+		new Athlete.Builder().setJavelinThrow(150.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void thousandFiveHundredMeterRaceTimeShouldBeZeroSecondsOrMore() {
+		new Athlete.Builder().setThousandFiveHundredMeterRace(-1.0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void thousandFiveHundredMeterRaceTimeShouldBeLessThan24Hours() {
+		new Athlete.Builder().setThousandFiveHundredMeterRace(ONE_DAY_IN_SECONDS);
+	}
 }
