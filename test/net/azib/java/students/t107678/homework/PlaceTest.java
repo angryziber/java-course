cb9ea7708e494fd2a9ed114c739d3c49a34b84d7
Radomir Sebek (107678) <rasho.ieee@gmail.com>
2011-05-19 19:13:10 +0000

    PlaceTest added

---
+++ test/net/azib/java/students/t107678/homework/PlaceTest.java
@@ -0,0 +1,28 @@
+package net.azib.java.students.t107678.homework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaceTest {
+
+    Place placeOne = new Place(1,0);
+    Place placeTwoToFour = new Place(2,2);
+    Place placeTenToFifteenth = new Place(10,5);
+
+
+    @Test
+    public void getSimplePlace() throws Exception {
+
+        assertEquals("1",placeOne.getFinalPlace());
+
+    }
+
+    @Test
+    public void getComplicatedPlace() throws Exception {
+
+        assertEquals("2-4",placeTwoToFour.getFinalPlace());
+        assertEquals("10-15",placeTenToFifteenth.getFinalPlace());
+
+    }
+}
