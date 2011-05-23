12d97bde4d0cff4da17816697da3c5128dda720b
Artjom Kruglenkov (092877) <artjom.kruglenkov@gmail.com>
2011-05-23 23:26:12 +0000

    ....

---
+++ test/net/azib/java/students/t092877/lecture3/FibonacciTest.java
@@ -0,0 +1,31 @@
+package net.azib.java.students.t092877.lecture3;
+
+
+import net.azib.java.students.t092877.lecture2.Fibonacci;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+
+public class FibonacciTest {
+
+	@Test
+	public void testFirstFourtyElementsInFibonacciSequence() {
+
+		Fibonacci fibonacci = new Fibonacci();
+
+		String fibonacciSequenceAsStr = "0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 " +
+										"1597 2584 4181 6765 10946 17711 28657 46368 75025 " +
+										"121393 196418 317811 514229 832040 1346269 2178309 " +
+										"3524578 5702887 9227465 14930352 24157817 39088169 63245986";
+
+		String[] fibonacciSequenceElements = fibonacciSequenceAsStr.split("\\s+");
+		BigInteger[] elements = new BigInteger[40];
+
+		for (int i = 0; i < fibonacciSequenceElements.length; i++) {
+			elements[i] = new BigInteger(fibonacciSequenceElements[i]);
+			assertEquals(elements[i], fibonacci.compute(i+1));
+		}
+	}
+}
