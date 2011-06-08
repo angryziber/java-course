8e10a106dda9a07dce5998e66063024c230315c1
Julia Kalitseva (100224) <julia.kalitseva@gmail.com>
2011-06-08 18:53:17 +0000

    unfinished homework

--- src/net/azib/java/students/t100224/homework/DecathlonMain.java
+++ src/net/azib/java/students/t100224/homework/DecathlonMain.java
@@ -8,7 +8,7 @@ public class DecathlonMain {
 	public static void main(String[] args) {
 		if (args.length > 2) {
 			new DecathlonController(args);
-			DOMConfigurator.configure("./log4j.xml");
+			DOMConfigurator.configure("./src/net/azib/java/students/t100224/homework/log4j.xml");
 		} else
 			System.err
 					.println("Usage: <program> -<input-method> [input-parameters] -<output-method> [output-parameters]");
